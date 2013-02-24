92674919c61f0be1b6b9b43f92af4352052c2b37
Andreas Wilm <wilma@gis.a-star.edu.sg>
2013-02-24 23:30:47 +0800

    Reindent
    Former-commit-id: b7e606b6bc4a45e3f06be68703d3e8b26e430912

--- src/lofreq_core/bam2depth.c
+++ src/lofreq_core/bam2depth.c
@@ -13,9 +13,9 @@
 
 
 typedef struct {     // auxiliary data structure
-	bamFile fp;      // the file handler
-	bam_iter_t iter; // NULL if a region not specified
-	int min_mapQ;    // mapQ filter
+     bamFile fp;      // the file handler
+     bam_iter_t iter; // NULL if a region not specified
+     int min_mapQ;    // mapQ filter
 } aux_t;
 
 void *bed_read(const char *fn); // read a BED or position list file
@@ -25,10 +25,10 @@ int bed_overlap(const void *_h, const char *chr, int beg, int end); // test if c
 // This function reads a BAM alignment from one BAM file.
 static int read_bam(void *data, bam1_t *b) // read level filters better go here to avoid pileup
 {
-	aux_t *aux = (aux_t*)data; // data in fact is a pointer to an auxiliary structure
-	int ret = aux->iter? bam_iter_read(aux->fp, aux->iter, b) : bam_read1(aux->fp, b);
-	if ((int)b->core.qual < aux->min_mapQ) b->core.flag |= BAM_FUNMAP;
-	return ret;
+     aux_t *aux = (aux_t*)data; // data in fact is a pointer to an auxiliary structure
+     int ret = aux->iter? bam_iter_read(aux->fp, aux->iter, b) : bam_read1(aux->fp, b);
+     if ((int)b->core.qual < aux->min_mapQ) b->core.flag |= BAM_FUNMAP;
+     return ret;
 }
 
 
@@ -41,108 +41,109 @@ int depth_stats(double *ret_mean, long int *ret_num_nonzero_pos,
                 char *usr_reg, char *usr_bed_file,
                 int *usr_baseQ, int *usr_mapQ)
 {
-	int i, tid, beg, end, pos, *n_plp, baseQ = 0, mapQ = 0;
-    const int n = 1;
-	const bam_pileup1_t **plp;
-	char *reg = 0; /* specified region */
-	void *bed = 0; /* BED data structure */
-	bam_header_t *h = 0; /* BAM header of the 1st input */
-	aux_t **data;
-	bam_mplp_t mplp;
+     int i, tid, beg, end, pos, *n_plp, baseQ = 0, mapQ = 0;
+     const int n = 1;
+     const bam_pileup1_t **plp;
+     char *reg = 0; /* specified region */
+     void *bed = 0; /* BED data structure */
+     bam_header_t *h = 0; /* BAM header of the 1st input */
+     aux_t **data;
+     bam_mplp_t mplp;
+     
+     long int num_nonzero_pos = 0;
+     double mean = 0.0; /* running average */
 
-    long int num_nonzero_pos = 0;
-    double mean = 0.0; /* running average */
 
+     if (NULL != usr_baseQ) {
+          baseQ = *usr_baseQ;
+     }
+     if (NULL != usr_mapQ) {
+          mapQ = *usr_mapQ;
+     }
+     if (NULL != usr_reg) {
+          reg = strdup(usr_reg);
+     }
+     assert(NULL != bam_file);
 
-    if (NULL != usr_baseQ) {
-       baseQ = *usr_baseQ;
-    }
-    if (NULL != usr_mapQ) {
-        mapQ = *usr_mapQ;
-    }
-    if (NULL != usr_reg) {
-         reg = strdup(usr_reg);
-    }
-    assert(NULL != bam_file);
+     if (0 != strcmp(bam_file, "-") && ! file_exists(bam_file)) {
+          fprintf(stderr, 
+                  "ERROR: BAM file does not exist: '%s'\n", 
+                  bam_file);
+          return -1;
+     }
+     if (NULL != usr_bed_file && ! file_exists(usr_bed_file)) {
+          fprintf(stderr, 
+                  "ERROR: given BED file does not exist: '%s'\n", 
+                  usr_bed_file);
+          return -1;
+     }
+     if (NULL != usr_bed_file) {
+          bed = bed_read(usr_bed_file);
+     }
 
-    if (0 != strcmp(bam_file, "-") && ! file_exists(bam_file)) {
-         fprintf(stderr, 
-                 "ERROR: BAM file does not exist: '%s'\n", 
-                 bam_file);
-         return -1;
-    }
-    if (NULL != usr_bed_file && ! file_exists(usr_bed_file)) {
-         fprintf(stderr, 
-                 "ERROR: given BED file does not exist: '%s'\n", 
-                 usr_bed_file);
-         return -1;
-    }
-    if (NULL != usr_bed_file) {
-         bed = bed_read(usr_bed_file);
-    }
+     /* initialize the auxiliary data structures
+        /* n = 1; // the number of BAMs on the command line */
+     /* AW: set n to 1 and changed argv[optindx+i] to bam_file to keep
+      * rest of code unchanged and c
+      */
 
-	// initialize the auxiliary data structures
-	/* n = 1; // the number of BAMs on the command line */
-    /* AW: set n to 1 and changed argv[optindx+i] to bam_file to keep
-     * rest of code unchanged and c*/
+     data = calloc(n, sizeof(void*)); // data[i] for the i-th input
+     beg = 0; end = 1<<30; tid = -1;  // set the default region
+     for (i = 0; i < n; ++i) {
+          bam_header_t *htmp;
+          data[i] = calloc(1, sizeof(aux_t));
+          data[i]->fp = bam_open(bam_file, "r"); // open BAM
+          data[i]->min_mapQ = mapQ;                    // set the mapQ filter
+          htmp = bam_header_read(data[i]->fp);         // read the BAM header
+          if (i == 0) {
+               h = htmp; // keep the header of the 1st BAM
+               if (reg) bam_parse_region(h, reg, &tid, &beg, &end); // also parse the region
+          } else bam_header_destroy(htmp); // if not the 1st BAM, trash the header
+          if (tid >= 0) { // if a region is specified and parsed successfully
+               bam_index_t *idx = bam_index_load(bam_file);  // load the index
+               data[i]->iter = bam_iter_query(idx, tid, beg, end); // set the iterator
+               bam_index_destroy(idx); // the index is not needed any more; phase out of the memory
+          }
+     }
 
-	data = calloc(n, sizeof(void*)); // data[i] for the i-th input
-	beg = 0; end = 1<<30; tid = -1;  // set the default region
-	for (i = 0; i < n; ++i) {
-		bam_header_t *htmp;
-		data[i] = calloc(1, sizeof(aux_t));
-		data[i]->fp = bam_open(bam_file, "r"); // open BAM
-		data[i]->min_mapQ = mapQ;                    // set the mapQ filter
-		htmp = bam_header_read(data[i]->fp);         // read the BAM header
-		if (i == 0) {
-			h = htmp; // keep the header of the 1st BAM
-			if (reg) bam_parse_region(h, reg, &tid, &beg, &end); // also parse the region
-		} else bam_header_destroy(htmp); // if not the 1st BAM, trash the header
-		if (tid >= 0) { // if a region is specified and parsed successfully
-			bam_index_t *idx = bam_index_load(bam_file);  // load the index
-			data[i]->iter = bam_iter_query(idx, tid, beg, end); // set the iterator
-			bam_index_destroy(idx); // the index is not needed any more; phase out of the memory
-		}
-	}
+     // the core multi-pileup loop
+     mplp = bam_mplp_init(n, read_bam, (void**)data); // initialization
+     n_plp = calloc(n, sizeof(int)); // n_plp[i] is the number of covering reads from the i-th BAM
+     plp = calloc(n, sizeof(void*)); // plp[i] points to the array of covering reads (internal in mplp)
+     while (bam_mplp_auto(mplp, &tid, &pos, n_plp, plp) > 0) { // come to the next covered position
+          if (pos < beg || pos >= end) continue; /* out of range; skip */
+          if (bed && bed_overlap(bed, h->target_name[tid], pos, pos + 1) == 0) continue; /* not in BED; skip */
+          /*fputs(h->target_name[tid], stdout); printf("\t%d", pos+1); // a customized printf() would be faster*/
+          for (i = 0; i < n; ++i) { // base level filters have to go here
+               int j, m = 0;
+               for (j = 0; j < n_plp[i]; ++j) {
+                    const bam_pileup1_t *p = plp[i] + j; // DON'T modfity plp[][] unless you really know
+                    if (p->is_del || p->is_refskip) ++m; // having dels or refskips at tid:pos
+                    else if (bam1_qual(p->b)[p->qpos] < baseQ) ++m; // low base quality
+               }
+              
+               num_nonzero_pos += 1;
+               mean += ((n_plp[i] - m) - mean) / num_nonzero_pos;
+               /*printf("\t%d", n_plp[i] - m); // this the depth to output*/
+          }
+          /*putchar('\n');*/
+     }
+     free(n_plp); free(plp);
+     bam_mplp_destroy(mplp);
 
-	// the core multi-pileup loop
-	mplp = bam_mplp_init(n, read_bam, (void**)data); // initialization
-	n_plp = calloc(n, sizeof(int)); // n_plp[i] is the number of covering reads from the i-th BAM
-	plp = calloc(n, sizeof(void*)); // plp[i] points to the array of covering reads (internal in mplp)
-	while (bam_mplp_auto(mplp, &tid, &pos, n_plp, plp) > 0) { // come to the next covered position
-        if (pos < beg || pos >= end) continue; /* out of range; skip */
-		if (bed && bed_overlap(bed, h->target_name[tid], pos, pos + 1) == 0) continue; /* not in BED; skip */
-		/*fputs(h->target_name[tid], stdout); printf("\t%d", pos+1); // a customized printf() would be faster*/
-		for (i = 0; i < n; ++i) { // base level filters have to go here
-			int j, m = 0;
-			for (j = 0; j < n_plp[i]; ++j) {
-				const bam_pileup1_t *p = plp[i] + j; // DON'T modfity plp[][] unless you really know
-				if (p->is_del || p->is_refskip) ++m; // having dels or refskips at tid:pos
-				else if (bam1_qual(p->b)[p->qpos] < baseQ) ++m; // low base quality
-			}
+     bam_header_destroy(h);
+     for (i = 0; i < n; ++i) {
+          bam_close(data[i]->fp);
+          if (data[i]->iter) bam_iter_destroy(data[i]->iter);
+          free(data[i]);
+     }
+     free(data); free(reg);
+     if (bed) bed_destroy(bed);
 
-            num_nonzero_pos += 1;
-            mean += ((n_plp[i] - m) - mean) / num_nonzero_pos;
-			/*printf("\t%d", n_plp[i] - m); // this the depth to output*/
-		}
-		/*putchar('\n');*/
-	}
-	free(n_plp); free(plp);
-	bam_mplp_destroy(mplp);
+     *ret_mean = mean;
+     *ret_num_nonzero_pos = num_nonzero_pos;
 
-	bam_header_destroy(h);
-	for (i = 0; i < n; ++i) {
-		bam_close(data[i]->fp);
-		if (data[i]->iter) bam_iter_destroy(data[i]->iter);
-		free(data[i]);
-	}
-	free(data); free(reg);
-	if (bed) bed_destroy(bed);
-
-    *ret_mean = mean;
-    *ret_num_nonzero_pos = num_nonzero_pos;
-
-	return 0;
+     return 0;
 }
 
 #ifdef BAM2DEPTH_MAIN
