25526ebeefe9944f9e9b4d5b620d6ecf2fb20668
Andreas Wilm <wilma@gis.a-star.edu.sg>
2013-02-24 23:26:29 +0800

    Initial commit. Logging functions now separate
    Former-commit-id: 804991c15429f67cf04a4bf464695b6738791950

---
+++ src/lofreq_core/log.c
@@ -0,0 +1,19 @@
+#include "log.h"
+
+int debug = 0;
+int verbose = 0;
+
+/* Taken from the Linux kernel source and slightly modified.
+ */
+int
+vout(FILE *stream, const char *fmt, ...)
+{                
+     va_list args;
+     int rc;
+     
+     va_start(args, fmt);
+     rc = vfprintf(stream, fmt, args);
+     va_end(args);
+     return rc;
+}
+
