25a42a3edcd5fa4394f8abb3f485b1c3480075c4
Andreas Wilm <wilma@gis.a-star.edu.sg>
2013-03-04 17:29:41 +0800

    - Renamed old python stuff to lofreq_python_prev - Added James Casbon's vcf.py and meta info
    Former-commit-id: ce3d66891b5d148e916927a2e3b94424d6a95925

--- src/lofreq_python/setup.py
+++ src/lofreq_python/setup.py
@@ -22,57 +22,11 @@ if sys.version_info >= (2 , 8):
                      " above 2.8 are not supported\n")
     sys.exit(1)
 
-    
-DEFINE_MACROS = []
-EXTRA_COMPILE_ARGS = []
-if not DEBUG:
-    DEFINE_MACROS.append(('NDEBUG', '1'))
-    EXTRA_COMPILE_ARGS.append('-O3')
-    EXTRA_COMPILE_ARGS.append('-funroll-loops')
-else:
-    EXTRA_COMPILE_ARGS.append('-Wall')
-    EXTRA_COMPILE_ARGS.append('-Wextra')
-    EXTRA_COMPILE_ARGS.append('-pedantic')
-    EXTRA_COMPILE_ARGS.append('-ansi')
-    EXTRA_COMPILE_ARGS.append('-O0')
-    EXTRA_COMPILE_ARGS.append('-g')
-    # NDEBUG seems to be always define through Python's OPT
-    EXTRA_COMPILE_ARGS.append('-UNDEBUG') 
-
-    
-# C extensions
-#
-#PYAPI_PATH = os.path.join("src", "ext")
-PYAPI_PATH = "lofreq_ext"
-PYAPI_SOURCES = [os.path.join(PYAPI_PATH, f)
-               for f in ["lofreq_ext.c"]]
-# The libraries to link against / to include
-extraobjects = []
-CDF_LIBDIR = os.path.join('..', 'cdflib90')
-CDF_LIB = os.path.join(CDF_LIBDIR, "libcdf.a")
-extraobjects.append(CDF_LIB)
-LOFREQCORE_LIBDIR = os.path.join('..', 'lofreq_core')
-LOFREQCORE_LIB = os.path.join(LOFREQCORE_LIBDIR, "liblofreq_core.a")
-extraobjects.append(LOFREQCORE_LIB)
-BAM_LIBDIR = os.path.join('..', 'libbam')
-BAM_LIB = os.path.join(BAM_LIBDIR, "libbam.a")
-extraobjects.append(BAM_LIB)
    
-extension = Extension(
-    "lofreq_ext",
-    PYAPI_SOURCES,
-    include_dirs=[CDF_LIBDIR, LOFREQCORE_LIBDIR],
-    define_macros=DEFINE_MACROS,
-    extra_compile_args=EXTRA_COMPILE_ARGS,
-    #extra_objects=[CDF_LIB, LOFREQCORE_LIB, BAM_LIB],
-    extra_objects=[LOFREQCORE_LIB, CDF_LIB, BAM_LIB],
-    #depends=[LOFREQCORE_LIB],
-    # libs statically linked using extra_objects instead of:
-    libraries=['z'],# library_dirs=[libdir],
-    )
-
 # where modules reside:
-package_dir = {'': setup_conf.PACKAGE_NAME.lower()}
+#package_dir = {'': setup_conf.PACKAGE_NAME.lower()}
+#package_dir = {'': ''}
+
     
 setup(name = setup_conf.PACKAGE_NAME,
       packages=[setup_conf.PACKAGE_NAME.lower()],
@@ -83,22 +37,10 @@ setup(name = setup_conf.PACKAGE_NAME,
       long_description = """LoFreq is a fast and sensitive variant-caller for inferring single-nucleotide variants (SNVs) from high-throughput sequencing data""",
       url='https://sourceforge.net/p/lofreq/',
       scripts = [
-          'scripts/lofreq_alnoffset.py',
-          'scripts/lofreq_bonf.py',
-          'scripts/lofreq_diff.py',
-          'scripts/lofreq_filter.py',
-          'scripts/lofreq_pileup_summary.py',
-          'scripts/lofreq_regionbed.py',
-          'scripts/lofreq_snpcaller.py',
-          'scripts/lofreq_uniq.py',
-          'scripts/lofreq_uniq_pipeline.py',
-          'scripts/lofreq_varpos_to_vcf.py',
-          'scripts/lofreq_version.py'
+          'scripts/lofreq2_filter.py',
       ],
-      ext_modules = [extension],
       # http://pypi.python.org/pypi?%3Aaction=list_classifiers
-      classifiers=['Development Status :: 4 - Beta',
-                   'Environment :: Console',
+      classifiers=['Environment :: Console',
                    'Intended Audience :: Science/Research',
                    'License :: OSI Approved :: GNU General Public License (GPL)',
                    'Natural Language :: English',
