504a1551ecbb6ae919552d840cd3cc106ebf8316
Andreas Wilm <wilma@gis.a-star.edu.sg>
2013-03-25 14:10:12 +0800

    Test update: moved old ones to tests.old to add one by one on demand. Currently only pylint.sh, doctest.sh and diff_opts_same_out.sh
    Former-commit-id: c961742ef6d2a538fbf3b8716e86cf0acc2c624e

---
+++ tests.old/vcf_output.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+echoerror() {
+    echo "ERROR: $1" 1>&2
+}
+echook() {
+    echo "OK: $1" 1>&2
+}
+echowarn() {
+    echo "WARN: $1" 1>&2
+}
+bam=../../lofreq-test-data/denv2-pseudoclonal/denv2-pseudoclonal.bam
+reffa=../../lofreq-test-data/denv2-pseudoclonal/denv2-pseudoclonal_cons.fa
+bed=../../lofreq-test-data/denv2-pseudoclonal/denv2-pseudoclonal_incl.bed
+vcf=$(basename $bam .bam).vcf
+
+# index bam if necessary
+test -s ${bam}.bai || samtools index $bam
+
+echowarn "Only checking that something's output but should check validity"
+lofreq_snpcaller.py -f $reffa -l $bed -b $bam -o $vcf --force --format vcf || exit 1
+#export  PERL5LIB=/Users/wilma/local/lib/
+if perl -mVcf -e validate ../tests/denv2-pseudoclonal.vcf; then
+    echook "Got valid VCF output"
+else
+    echoerror "Invalid VCF output"
+fi
