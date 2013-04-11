93264ad069491c9bec487b435a860c8c26296e0d
Andreas Wilm <wilma@gis.a-star.edu.sg>
2013-04-11 11:08:56 +0800

    Added LOG_NOTE

--- src/lofreq_core/log.h
+++ src/lofreq_core/log.h
@@ -11,16 +11,18 @@ int
 vout(FILE *stream, const char *fmt, ...);
 
 /* print only if debug is true*/
-#define LOG_DEBUG(fmt, args...)     {if (debug) {(void)vout(stderr, "DEBUG(%s|%s): " fmt, __FILE__, __FUNCTION__, ## args);}}
+#define LOG_DEBUG(fmt, args...)    {if (debug) {(void)vout(stderr, "DEBUG(%s|%s): " fmt, __FILE__, __FUNCTION__, ## args);}}
 /* print only if verbose is true*/
-#define LOG_VERBOSE(fmt, args...)      {if (verbose) {(void)vout(stderr, fmt, ## args);}}
+#define LOG_VERBOSE(fmt, args...)  {if (verbose) {(void)vout(stderr, fmt, ## args);}}
 /* always warn to stderr */
-#define LOG_WARN(fmt, args...)      (void)vout(stderr, "WARNING(%s|%s): " fmt, __FILE__, __FUNCTION__, ## args)
+#define LOG_WARN(fmt, args...)     (void)vout(stderr, "WARNING(%s|%s): " fmt, __FILE__, __FUNCTION__, ## args)
 /* always print errors to stderr*/
-#define LOG_ERROR(fmt, args...)     (void)vout(stderr, "ERROR(%s|%s:%d): " fmt, __FILE__, __FUNCTION__, __LINE__, ## args)
+#define LOG_ERROR(fmt, args...)    (void)vout(stderr, "ERROR(%s|%s:%d): " fmt, __FILE__, __FUNCTION__, __LINE__, ## args)
 /* always print errors to stderr*/
-#define LOG_FATAL(fmt, args...)     (void)vout(stderr, "FATAL(%s|%s:%d): " fmt, __FILE__, __FUNCTION__, __LINE__, ## args)
+#define LOG_FATAL(fmt, args...)    (void)vout(stderr, "FATAL(%s|%s:%d): " fmt, __FILE__, __FUNCTION__, __LINE__, ## args)
 /* always print fixme's */
-#define LOG_FIXME(fmt, args...)     (void)vout(stderr, "FIXME(%s|%s:%d): " fmt, __FILE__, __FUNCTION__, __LINE__, ## args)
+#define LOG_FIXME(fmt, args...)    (void)vout(stderr, "FIXME(%s|%s:%d): " fmt, __FILE__, __FUNCTION__, __LINE__, ## args)
+/* always print notes */
+#define LOG_NOTE(fmt, args...)     (void)vout(stderr, "NOTE(%s|%s:%d): " fmt, __FILE__, __FUNCTION__, __LINE__, ## args)
 
 #endif
