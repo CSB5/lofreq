f18b7dc641b493fdfc191ce20056ba3cec8b201f
Andreas Wilm - CMB5 <wilma@gis.a-star.edu.sg>
2013-04-11 12:49:37 +0800

    Now using shared lib.sh

--- tests/vcf_output.sh
+++ tests/vcf_output.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 
-echoerror() {
-    echo "ERROR: $1" 1>&2
-}
-echook() {
-    echo "OK: $1" 1>&2
-}
-echowarn() {
-    echo "WARN: $1" 1>&2
-}
+source lib.sh || exit 1
+
 bam=../../lofreq-test-data/denv2-pseudoclonal/denv2-pseudoclonal.bam
 reffa=../../lofreq-test-data/denv2-pseudoclonal/denv2-pseudoclonal_cons.fa
 bed=../../lofreq-test-data/denv2-pseudoclonal/denv2-pseudoclonal_incl.bed
