108d59805e4c39ed075c6798058182b724a85240
Andreas Wilm - CMB5 <wilma@gis.a-star.edu.sg>
2013-04-22 18:33:16 +0800

    Updated to lofreq2/star

---
+++ tests/valid_vcf_output.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+source lib.sh || exit 1
+
+bam=./data/denv2-pseudoclonal/denv2-pseudoclonal.bam
+reffa=./data/denv2-pseudoclonal/denv2-pseudoclonal_cons.fa
+bed=./data/denv2-pseudoclonal/denv2-pseudoclonal_incl.bed
+vcf=$(mktemp -t $(basename $0).XXXXXX.vcf)
+rm -f $vcf
+
+# index bam if necessary
+test -s ${bam}.bai || samtools index $bam
+
+$LOFREQ call -f $reffa -l $bed -b auto -o $vcf $bam || exit 1
+#export  PERL5LIB=/Users/wilma/local/lib/
+if perl -mVcf -e validate ../tests/denv2-pseudoclonal.vcf; then
+    echook "Got valid VCF output"
+else
+    echoerror "Invalid VCF output"
+fi
