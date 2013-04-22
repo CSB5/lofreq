597aba184f72a295456b718c32e84f6711276650
Andreas Wilm - CMB5 <wilma@gis.a-star.edu.sg>
2013-04-22 17:54:40 +0800

    - Added frontend to lofreq2_somatic.py - Added own argv0 dir to PATH (in case we call a script that calls argv0 again as is the case for lofreq2_somatic.py)
    Former-commit-id: f1ad402be46665ebcd0ad182260043d126600e34

--- src/lofreq/lofreq_main.c
+++ src/lofreq/lofreq_main.c
@@ -19,18 +19,18 @@
 
 
 
-/* prepend python source dir to PATH making sure that package works
- * even without properly installing it 
-*/
+/* prepend dirname(argv0) and python source dir to PATH making sure
+ * that package works even without properly installing it
+ */
 void 
-add_src_dir_to_path(char *argv0) {
-     const char *path_name = "PATH";
+add_local_dir_to_path(char *argv0) {
+     const char *PATH_NAME = "PATH";
      char *path_var = NULL;
      char *old_path = NULL;
      char *argv0_cp = NULL;
      char *dirname_argv0 = NULL;
-     char lofreq2_script_rel[] = "../lofreq_python/scripts/lofreq2_filter.py";
-     char *lofreq2_script_abs = NULL;
+     char lofreq_script_rel[] = "../lofreq_python/scripts/lofreq2_filter.py";
+     char *lofreq_script_abs = NULL;
 
      argv0_cp = strdup(argv0); /* dirname might change contents */
      if (NULL == (dirname_argv0 = strdup(dirname(argv0_cp)))) {
@@ -38,43 +38,56 @@ add_src_dir_to_path(char *argv0) {
           return;
      }
 
-     if (NULL == (lofreq2_script_abs = strdup(dirname_argv0))) {
+     if (NULL == (lofreq_script_abs = strdup(dirname_argv0))) {
           free(dirname_argv0);
           free(argv0_cp);
           return;
      }
 
-     if (NULL == join_paths(&lofreq2_script_abs, lofreq2_script_rel)) {
-          free(lofreq2_script_abs);
+     if (NULL == join_paths(&lofreq_script_abs, lofreq_script_rel)) {
+          free(lofreq_script_abs);
           free(dirname_argv0);
           free(argv0_cp);
           return;          
      }
      /* check also done by realpath in join_path but doesn't hurt to
       * check again */
-     if (! file_exists(lofreq2_script_abs)) {
-          free(lofreq2_script_abs);
+     if (! file_exists(lofreq_script_abs)) {
+          free(lofreq_script_abs);
           free(dirname_argv0);
           free(argv0_cp);
           return;
      }
 
-     path_var = strdup(dirname(lofreq2_script_abs));
-     LOG_NOTE("Adding local source directory %s to PATH\n", path_var);
+     path_var = strdup(dirname(lofreq_script_abs));
+     LOG_VERBOSE("Adding local source directory %s to PATH\n", path_var);
 
-     old_path = getenv(path_name);
+     old_path = getenv(PATH_NAME);
      if (NULL == old_path) {
-          setenv(path_name, path_var, 1);
+          setenv(PATH_NAME, path_var, 1);
      } else {
           path_var = realloc(
                path_var, (strlen(path_var) + 1 + strlen(old_path) + 1) * sizeof(char));
           (void) strcat(path_var, ":");
           (void) strcat(path_var, old_path);
-          setenv(path_name, path_var, 1);
+          setenv(PATH_NAME, path_var, 1);
      }
 
+
      free(path_var);
-     free(lofreq2_script_abs);
+     old_path = getenv(PATH_NAME);
+     path_var = strdup(dirname(argv0));
+     LOG_VERBOSE("Adding argv0 directory %s to PATH\n", path_var);
+     path_var = realloc(
+          path_var, (strlen(path_var) + 1 + strlen(old_path) + 1) * sizeof(char));
+     (void) strcat(path_var, ":");
+     (void) strcat(path_var, old_path);
+     setenv(PATH_NAME, path_var, 1);
+     LOG_DEBUG("New PATH = %s\n", path_var);
+
+
+     free(path_var);
+     free(lofreq_script_abs);
      free(dirname_argv0);
      free(argv0_cp);
 }
@@ -87,6 +100,7 @@ static void usage(const char *myname)
      fprintf(stderr, "\n");
      fprintf(stderr, "Usage: %s <command> [options], where command is one of:\n", myname);
      fprintf(stderr, "  call        : call variants\n");
+     fprintf(stderr, "  somatic     : call somatic variants\n");
      fprintf(stderr, "  filter      : filter variants\n");
      fprintf(stderr, "  plp_summary : FIXME:finish print pileup summary\n");
      fprintf(stderr, "  version     : prints version\n");
@@ -96,7 +110,7 @@ static void usage(const char *myname)
 
 int main(int argc, char *argv[])
 {
-     add_src_dir_to_path(argv[0]);
+     add_local_dir_to_path(argv[0]);
 
      if (argc < 2) {
           usage(BASENAME(argv[0]));
@@ -114,6 +128,23 @@ int main(int argc, char *argv[])
           }
           argv_execvp[i-1] = NULL; /* sentinel */
           if (execvp("lofreq2_filter.py", argv_execvp)) {
+               perror("Calling lofreq2_filter.py via execvp failed");
+               free(argv_execvp);
+               return -1;
+          } else {
+               free(argv_execvp);
+               return 0;
+          }
+
+     } else if (strcmp(argv[1], "somatic") == 0) {
+          char **argv_execvp = calloc(argc, sizeof(char*));
+          int i;
+          argv_execvp[0] = argv[0];
+          for (i=2; i<argc; i++) {
+               argv_execvp[i-1] = argv[i];
+          }
+          argv_execvp[i-1] = NULL; /* sentinel */
+          if (execvp("lofreq2_somatic.py", argv_execvp)) {
                perror("Calling lofreq2_filter.py via execvp failed");
                free(argv_execvp);
                return -1;
