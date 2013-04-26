a0db6a69e5e7038ba04f2f3cedfcd7361798353b
Andreas Wilm <wilma@gis.a-star.edu.sg>
2013-04-26 22:35:11 +0800

    Now adding path of main c binary to PATH

--- src/lofreq_python/scripts/lofreq2_local.py
+++ src/lofreq_python/scripts/lofreq2_local.py
@@ -1,12 +1,20 @@
 # Add local dir to path to make source dir, i.e. not installed scripts
 # work straight-away
 
-import sys, os
+import sys
+import os
 
-d = os.path.join(
-    os.path.dirname(sys.argv[0]), '..')
+d = os.path.normpath(os.path.join(
+    os.path.dirname(sys.argv[0]), '..'))
 if os.path.exists(os.path.join(d, 'lofreq_star')):
     sys.stderr.write("NOTE: Adding local dir %s to PYTHONPATH\n" % d)
     sys.path.insert(0, d)
+
+d = os.path.normpath(os.path.join(
+    os.path.dirname(sys.argv[0]), '../../lofreq'))
+if os.path.exists(os.path.join(d, 'lofreq')):
+    sys.stderr.write("NOTE: Adding local dir %s to PATH\n" % d)
+    os.environ["PATH"] = os.pathsep + d + os.environ["PATH"]
+    
 
         
