d8dc16cc714f76db61f63f2a8152dc510b6e00cf
Andreas Wilm - CMB5 <wilma@gis.a-star.edu.sg>
2013-04-29 13:37:43 +0800

    Fixed wrong PYTHONPATH usage
    Former-commit-id: 9bc9776ac83ad20adb878d8dd6c936821d7d6e14

--- src/lofreq_python/scripts/lofreq2_local.py
+++ src/lofreq_python/scripts/lofreq2_local.py
@@ -4,17 +4,26 @@
 import sys
 import os
 
+# Set sys.path/PYTHONPATH such that we find the local source dir first
 d = os.path.normpath(os.path.join(
-    os.path.dirname(sys.argv[0]), '..'))
-if os.path.exists(os.path.join(d, 'lofreq_star')):
-    sys.stderr.write("NOTE: Adding local dir %s to PYTHONPATH\n" % d)
+    os.path.dirname(sys.argv[0]), '../lofreq_star/'))
+if os.path.exists(d):
+    #sys.stderr.write("NOTE: Adding local dir %s to PYTHONPATH\n" % d)
     sys.path.insert(0, d)
 
+# Set PATH such that we find lofreq binary first
 d = os.path.normpath(os.path.join(
     os.path.dirname(sys.argv[0]), '../../lofreq'))
 if os.path.exists(os.path.join(d, 'lofreq')):
-    sys.stderr.write("NOTE: Adding local dir %s to PATH\n" % d)
-    os.environ["PATH"] = os.pathsep + d + os.environ["PATH"]
+    #sys.stderr.write("NOTE: Adding local dir %s to PATH\n" % d)
+    os.environ["PATH"] = d + os.pathsep + os.environ["PATH"]
+
+# No need to find scripts because the main binary knows about them
+#d = os.path.normpath(os.path.join(
+#    os.path.dirname(sys.argv[0]), '../scripts'))
+#if os.path.exists(d):
+#    sys.stderr.write("NOTE: Adding local dir %s to PATH\n" % d)
+#    os.environ["PATH"] = os.pathsep + d + os.environ["PATH"]
     
 
         
