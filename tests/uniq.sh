7620c93bc518cd15743769112183f0f40ea33c09
Andreas Wilm <wilma@gis.a-star.edu.sg>
2013-05-07 18:36:26 +0800

    added uniq check against indels and filtered variants
    Former-commit-id: 6be4060a5db5e442398c97b0248a10eca0262fdc

--- tests/uniq.sh
+++ tests/uniq.sh
@@ -21,3 +21,15 @@ else
     echook "Got zero SNVs during self-comparison, as expected"
 fi
 rm $vcf_out
+
+
+vcf_in=data/vcf/denv2-10haplo-fake-filter-only-and-indels.vcf
+$LOFREQ uniq -v $vcf_in $bam -o $vcf_out || exit 1
+num_snvs=$(grep -cv '^#' $vcf_out)
+if [ "$num_snvs" -ne 0 ]; then
+    echoerror "Expected zero SNVs when checking against indels and filtered variants only but got $num_snvs"
+    exit 1
+else
+    echook "Got zero SNVs when checking indels and filtered variants only"
+fi
+rm $vcf_out
