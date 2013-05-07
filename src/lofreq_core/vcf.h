03f9d0a41deb9b0f253275bc7a10cf956e6119c8
Andreas Wilm <wilma@gis.a-star.edu.sg>
2013-05-07 18:03:48 +0800

    Added reading routines
    Former-commit-id: 079663f7916020be70a73f614d7892bad8d62c5d

--- src/lofreq_core/vcf.h
+++ src/lofreq_core/vcf.h
@@ -16,25 +16,35 @@ typedef struct {
      int qual;
      char *filter;
      char *info;
+
+     /* genotyping info (not used in lofreq) */
+     char *format;
+     int num_samples;
+     char **samples;
 } var_t;
 
 typedef struct {
-      int ref_fw;
-      int ref_rv;
-      int alt_fw;
-      int alt_rv;
+     int ref_fw;
+     int ref_rv;
+     int alt_fw;
+     int alt_rv;
 } dp4_counts_t;
 
 
-void vcf_write_header(FILE *stream, const char *srcprog, const char *reffa);
-
 void vcf_new_var(var_t **var);
 void vcf_free_var(var_t **var);
+
+int vcf_parse_header(char **header, FILE *stream);
+int vcf_parse_vars(FILE *stream, var_t ***vars);
+
+int
+vcf_var_has_info_key(char **value, const var_t *var, const char *key);
 
 void vcf_var_sprintf_info(var_t *var,
                           const int *dp, const float *af, const int *sb,
                           const dp4_counts_t *dp4,
                           const int is_indel, const int is_consvar);
 void vcf_write_var(FILE *stream, const var_t *var);
+void vcf_write_header(FILE *stream, const char *srcprog, const char *reffa);
 
 #endif
