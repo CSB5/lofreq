eee80e236b5763ef18146d296a381da67694a3a2
Andreas Wilm - CMB5 <wilma@gis.a-star.edu.sg>
2013-05-09 09:33:09 +0800

    - binom_sf now binom and returning sf and/or cdf - Added main test routine

--- src/lofreq_core/binom.h
+++ src/lofreq_core/binom.h
@@ -1,18 +1,15 @@
+/* -*- c-file-style: "k&r"; indent-tabs-mode: nil; -*- */
+#ifndef BINOM_H
+#define BINOM_H
+
 /*********************************************************************
  *
- * Copyright (C) 2011, 2012 Genome Institute of Singapore
+ * FIXME license update
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the GNU
- * General Public License for more details.
  *
  *********************************************************************/
 
-int binom_sf(double *q, int num_trials, int num_successes,
-             double prob_success);
+int binom(double *q, double *p,
+          int num_trials, int num_successes, double prob_success);
+
+#endif
