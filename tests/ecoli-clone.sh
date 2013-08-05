02316e9ba90904efeef0262fc47f234c6fcfe37f
Andreas Wilm <wilma@gis.a-star.edu.sg>
2013-08-05 15:34:09 +0800

    Added test script for clonal data and aso added data
    Former-commit-id: 12acccca9cc4b88b8c0d7f67a79d85d5af57bbb5

---
+++ tests/ecoli-clone.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+# Test that we get the number of expected SNVs on the pseudo-clonal data-set
+
+source lib.sh || exit 1
+
+
+basedir=data/ecoli-clone/
+bam=$basedir/Ecoli_K12_MG1655_NC_000913_bwa-sampe-unique-rg_recal.bam
+reffa=$basedir/Ecoli_K12_MG1655_NC_000913.fa.gz
+#truesnv=$basedir/denv2-pseudoclonal_true-snp.vcf
+
+outdir=$(mktemp -d -t $(basename $0).XXXXXX)
+outvcf=$outdir/$(basename $bam .bam).vcf
+log=$outdir/log.txt
+
+KEEP_TMP=0
+
+cmd="$LOFREQ call -f $reffa -E -o $outvcf $bam"
+if ! eval $cmd >> $log 2>&1; then
+    echoerror "The following command failed (see $log for more): $cmd"
+    exit 1
+fi
+
+
+MAX_SNVS=20
+nsnvs=$(grep -c '^[^#]' $outvcf)
+if [ $nsnvs -ge $MAX_SNVS ]; then
+    echoerror "Expected less then $MAX_SNVS on this clonal dataset"
+    exit 1
+else
+    echook "Got $nsnvs SNVs for this clonal dataset which is okay (below limit of $MAX_SNVS)"
+fi
+
+
+
+if [ $KEEP_TMP -eq 1 ]; then
+    echowarn "Not deleting tmp dir $outdir"
+else 
+    rm  $outdir/*
+    rmdir $outdir
+fi
+
+
+if [ $KEEP_TMP -eq 1 ]; then
+    echowarn "Not deleting tmp dir $outdir"
+else 
+    rm  $outdir/*
+    rmdir $outdir
+fi
+
