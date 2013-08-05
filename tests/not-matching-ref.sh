2d38a34f87ec7e38c467b0be8d0122157a65d1f6
Andreas Wilm <wilma@gis.a-star.edu.sg>
2013-08-05 14:10:47 +0800

    Added test to make sure we can only run with proper ref
    Former-commit-id: 688dc8f5809b046ce57564a91d011179e0ae581f

---
+++ tests/not-matching-ref.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Test whether we can detect if the wrong reference was given
+
+source lib.sh || exit 1
+
+
+bam=data/denv2-pseudoclonal/denv2-pseudoclonal.bam
+reffa=data/denv2-simulation/denv2-refseq.fa
+
+
+cmd="$LOFREQ call -f $reffa $bed $bam"
+if eval $cmd 2>/dev/null; then
+    echoerror "LoFreq should have failed but didn't. Command was $cmd"
+    exit 1
+fi
+
