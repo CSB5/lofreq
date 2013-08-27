997e87afc3c3f751bf415255c3fdd1fcc988a924
Andreas Wilm <wilma@gis.a-star.edu.sg>
2013-08-27 21:04:36 +0800

    Changes to reflect new lofreq_somatic.py usage

--- tests/somatic.sh
+++ tests/somatic.sh
@@ -5,7 +5,7 @@
 source lib.sh || exit 1
 
 
-KEEP=1
+KEEP=0
 
 bam_n=./data/somatic/CHH966-normal-100x-100pur-hg19.chr22-bed-only.bam
 bam_t=./data/somatic/CHH966-tumor-100x-10pur-hg19.chr22-bed-only.bam
@@ -14,10 +14,8 @@ reffa=./data/somatic/hg19_chr22.fa.gz
 truesnv=./data/somatic/hg19_chr22_true_snv.vcf
 #outprefix=$(mktemp -t $(basename $0).XXXXXX)
 outprefix=$(mktemp -t $(basename $0));#XXXXXX needed on linux?
-finalout=${outprefix}lofreq_somatic_final.vcf
-#$LOFREQ somatic -n $bam_n -t $bam_t -f $reffa -l $bed -o $outprefix || exit 1
-#$LOFREQ somatic -S 10 -n $bam_n -t $bam_t -f $reffa -l $bed -o $outprefix || exit 1
-$LOFREQ somatic -F 0.01 -n $bam_n -t $bam_t -f $reffa -l $bed -o $outprefix || exit 1
+finalout=${outprefix}somatic_final.vcf
+$LOFREQ somatic -n $bam_n -t $bam_t -f $reffa -l $bed -o $outprefix || exit 1
 n_intersect=$($LOFREQ vcfset -1 $truesnv -2 $finalout -a intersect | grep -vc '^#')
 if [ "$n_intersect" -lt 2 ]; then
 	echoerror "Expected at least two true predictions but got $n_intersect (compare $finalout and $truesnv)"
