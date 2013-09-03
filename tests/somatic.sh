182782db1171f7cdc83ebb28d30270844d31cc4e
Andreas Wilm - CMB5 <wilma@gis.a-star.edu.sg>
2013-09-03 14:54:08 +0800

    mktemp fix that should work on mac and linux

--- tests/somatic.sh
+++ tests/somatic.sh
@@ -12,8 +12,7 @@ bam_t=./data/somatic/CHH966-tumor-100x-10pur-hg19.chr22-bed-only.bam
 bed=./data/somatic/SeqCap_EZ_Exome_v3_primary_lib_extend_no_overlap_minus300.chr22.bed
 reffa=./data/somatic/hg19_chr22.fa.gz
 truesnv=./data/somatic/hg19_chr22_true_snv.vcf
-#outprefix=$(mktemp -t $(basename $0).XXXXXX)
-outprefix=$(mktemp -t $(basename $0));#XXXXXX needed on linux?
+outprefix=$(mktemp -t $(basename $0) 2>/dev/null || mktemp -t $(basename $0).XXXXXX);#XXXXXX needed on linux?
 finalout=${outprefix}somatic_final.vcf
 $LOFREQ somatic -n $bam_n -t $bam_t -f $reffa -l $bed -o $outprefix || exit 1
 n_intersect=$($LOFREQ vcfset -1 $truesnv -2 $finalout -a intersect | grep -vc '^#')
