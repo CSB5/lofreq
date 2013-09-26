13be68d176a4b2949e22295a973aaf1590b80449
Andreas Wilm <wilma@gis.a-star.edu.sg>
2013-09-26 10:25:18 +0800

    Now fully funtional
    Former-commit-id: ec5711b649827931f18330d5d8152a037593c427

--- src/lofreq/lofreq_bamstats.c
+++ src/lofreq/lofreq_bamstats.c
@@ -7,11 +7,13 @@
  *
  *********************************************************************/
 
+/* loosely based on 0.1.18 sam_view.c */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <getopt.h>
+#include <assert.h>
 
 /* samtools includes */
 #include "sam.h"
@@ -33,6 +35,7 @@ int bed_overlap(const void *_h, const char *chr, int beg, int end);
 #define MYNAME PACKAGE
 #endif
 
+#define MAX_OP_COUNT 1024
 
 
 typedef struct {
@@ -44,6 +47,8 @@ typedef struct {
      int samflags_on;
      int samflags_off;
 } bamstats_conf_t;
+
+
 
 /* sam_view.c:__g_skip_aln */
 static inline int skip_aln(const bam_header_t *h, const bam1_t *b,
@@ -81,6 +86,126 @@ usage(bamstats_conf_t *bamstats_conf)
 /* usage() */
 
 
+void
+writestats(char *target_name, unsigned long int **read_cat_counts, unsigned long int num_reads, FILE *out)
+{
+     int i, j;
+     fprintf(out, "#chrom\top-category\top-count\tproportion of reads\n");
+     fprintf(out, "#proportions are in scientific notation or missing altogether if no reads for that count were found\n");
+     for (i=0; i<NUM_OP_CATS; i++) {
+          unsigned long int cat_sum = 0;
+          for (j=0; j<MAX_OP_COUNT; j++) {
+               if (read_cat_counts[i][j]) {
+                    fprintf(out, "%s\t%s\t%d\t%g\t(%lu/%lu)\n", 
+                            target_name, op_cat_str[i], j, read_cat_counts[i][j]/(double)num_reads, read_cat_counts[i][j], num_reads);
+                    cat_sum += read_cat_counts[i][j];
+               }
+          }
+          if (cat_sum != num_reads) {
+               LOG_FIXME("fail cat_sum=%lu != num_reads=%lu\n", cat_sum, num_reads);
+          }
+     }
+}
+
+
+int 
+bamstats(samfile_t *sam, bamstats_conf_t *bamstats_conf)
+{
+     char *target_name = NULL; /* chrom name */
+     char *ref = NULL; /* reference sequence */
+     unsigned long int **read_cat_counts;
+     unsigned long int num_good_reads = 0;
+     unsigned long int num_ign_reads = 0;
+     unsigned int num_zero_matches = 0;
+     bam1_t *b = bam_init1();
+     int r, i, rc;
+
+     /*  count_matches/read_cat_counts assume roughtly equal read length */
+     LOG_WARN("%s\n", "assuming (roughly) equal read length");
+
+     read_cat_counts = calloc(NUM_OP_CATS, sizeof(unsigned long int *));
+     for (i=0; i<NUM_OP_CATS; i++) {
+          read_cat_counts[i] = calloc(MAX_OP_COUNT, sizeof(unsigned long int));
+     }
+     
+     while ((r = samread(sam, b)) >= 0) { // read one alignment from `in'
+          int counts[NUM_OP_CATS];
+          int ref_len = -1;
+          
+          if (skip_aln(sam->header, b, bamstats_conf->min_mq, 
+                       bamstats_conf->samflags_on, bamstats_conf->samflags_off,
+                       bamstats_conf->bed)) {
+               num_ign_reads += 1;
+               continue;
+          }
+          num_good_reads += 1;
+          
+          if (0 == (num_good_reads+num_ign_reads)%1000000) {
+               LOG_VERBOSE("Still alive and happily crunching away on read number %d\n", (num_good_reads+num_ign_reads));
+          }
+
+          /* load ref only if necessary. also triggers output of
+           * stats per chrom */
+          if (ref == NULL || strcmp(target_name, sam->header->target_name[b->core.tid]) != 0) {
+               if (ref) {
+                    /* we already have some stats:  output */
+                    writestats(target_name, read_cat_counts, num_good_reads, stdout);
+
+                    /* reset everything for next chrom... */
+                    for (i=0; i<NUM_OP_CATS; i++) {
+                         memset(read_cat_counts[i], 0, MAX_OP_COUNT * sizeof(unsigned long int));
+                    }
+                    free(ref);
+               } 
+
+               target_name = sam->header->target_name[b->core.tid];
+               ref = faidx_fetch_seq(bamstats_conf->fai, target_name,
+                                     0, 0x7fffffff, &ref_len);
+          }
+
+          if (count_matches(counts, b, ref, bamstats_conf->min_mq)) {
+               LOG_WARN("%s\n", "count_matches failed on read. ignoring"); /* FIXME print read */
+               continue;
+          }
+          for (i=0; i<NUM_OP_CATS; i++) {
+               assert(counts[i]<MAX_OP_COUNT);               
+               read_cat_counts[i][counts[i]] += 1;
+          }
+          if (0 == counts[OP_MATCH]) {
+               LOG_DEBUG("Got read with zero matches after filtering with min_mq %d: name:%s cigar:%s qual:%s\n", 
+                        bamstats_conf->min_mq, bam1_qname(b), cigar_str_from_bam(b), bam1_qual(b));
+               num_zero_matches += 1;
+          }
+#if 0
+          LOG_DEBUG("good/ign=%u/%u: m=%d mm=%d i=%d d=%d\n", num_good_reads, num_ign_reads,
+                    counts[OP_MATCH], counts[OP_MISMATCH], counts[OP_INS], counts[OP_DEL]);               
+#endif
+     }
+               
+     /* don't forget to output last seen chrom */
+     if (ref) {
+          writestats(target_name, read_cat_counts, num_good_reads, stdout);
+          free(ref);
+     }
+
+     LOG_VERBOSE("#reads with zero matches (after bq filtering): %u\n", num_zero_matches);
+     LOG_VERBOSE("#reads ignored for counting (due to bed/mq filtering): %u\n", num_ign_reads);
+     LOG_VERBOSE("#reads used for counting: %u\n", num_good_reads);
+     
+     for (i=0; i<NUM_OP_CATS; i++) {
+          free(read_cat_counts[i]);
+     }
+     free(read_cat_counts);
+     
+     if (r < -1) {
+          LOG_FATAL("%s\n", "BAM file is truncated.\n");
+          rc = 1;
+     } else {
+          rc = 0;
+          bam_destroy1(b);
+     }
+     return rc;
+}
 
 
 int 
@@ -90,8 +215,6 @@ main_bamstats(int argc, char *argv[])
      char *bamfile = NULL;
      char *bedfile = NULL;
      samfile_t *sam =  NULL;
-     long int num_good_reads = 0;
-     long int num_ign_reads = 0;
      int rc = 0;
      bamstats_conf_t bamstats_conf;
 
@@ -145,7 +268,8 @@ main_bamstats(int argc, char *argv[])
 
           case 'h': 
                usage(&bamstats_conf); 
-               return 0;
+               rc = 0;
+               goto free_and_exit;
 
           case 'l': 
               bedfile = strdup(optarg);
@@ -155,8 +279,8 @@ main_bamstats(int argc, char *argv[])
                bamstats_conf.fa = strdup(optarg);
                bamstats_conf.fai = fai_load(optarg);
                if (bamstats_conf.fai == 0)  {
-                    free(bamstats_conf.fa);
-                    return 1;
+                    rc = 1;
+                    goto free_and_exit;
                }
                break;
 
@@ -164,7 +288,8 @@ main_bamstats(int argc, char *argv[])
                if (0 != strcmp(optarg, "-")) {
                     if (file_exists(optarg)) {
                          LOG_FATAL("Cowardly refusing to overwrite file '%s'. Exiting...\n", optarg);
-                         return 1;
+                         rc = 1;
+                         goto free_and_exit;
                     }
                } 
                outfile = strdup(optarg);
@@ -180,7 +305,8 @@ main_bamstats(int argc, char *argv[])
 
          case '?': 
                LOG_FATAL("%s\n", "Unrecognized arguments found. Exiting...\n"); 
-               return 1;
+               rc = 1;
+               goto free_and_exit;
                
           default:
                break;
@@ -191,27 +317,31 @@ main_bamstats(int argc, char *argv[])
      if (argc == 2) {
           fprintf(stderr, "\n");
           usage(&bamstats_conf);
-          return 1;
+          rc = 1;
+          goto free_and_exit;
      }
      
      if (1 != argc - optind - 1) {
           LOG_FATAL("%s\n\n", "Need exactly one BAM file as last argument");
-          return 1;
+          rc = 1;
+          goto free_and_exit;
      }
      bamfile = (argv + optind + 1)[0];
      if (!file_exists(bamfile)) {
           LOG_FATAL("BAM file %s does not exist.\n\n", bamfile);
-          return -1;
+          rc = 1;
+          goto free_and_exit;
      }
      
      if (NULL == bamstats_conf.fa) {
           LOG_FATAL("%s\n\n", "ERROR: Missing reference fasta argument");
           usage(&bamstats_conf);
-          return 1;
+          rc = 1;
+          goto free_and_exit;
      }
 
      if (bedfile) {
-          LOG_VERBOSE("%s\n", "Bed routines don't make use of indexing, i.e. read BAM file fully and are therefore slow."); /* FIXME */
+          LOG_VERBOSE("%s\n", "NOTE: bed routines don't make use of indexing and are therefore as slow as reading the whole BAM file."); /* FIXME */
           bamstats_conf.bed = bed_read(bedfile);
      }
 
@@ -220,56 +350,13 @@ main_bamstats(int argc, char *argv[])
      if ((sam = samopen(bamfile, "rb", NULL)) == 0) {
           LOG_FATAL("Failed to open \"%s\" for reading.\n", bamfile);
           rc = 1;
-
      } else {
-          /* partially based on 0.1.18 sam_view.c */
-          bam1_t *b = bam_init1();
-          int r;
-          char *target_name = NULL;
-          char *ref = NULL;
-
-          while ((r = samread(sam, b)) >= 0) { // read one alignment from `in'
-               int counts[MAX_COUNT_IDX];
-               int ref_len = -1;
-
-               if (skip_aln(sam->header, b, bamstats_conf.min_mq, 
-                              bamstats_conf.samflags_on, bamstats_conf.samflags_off,
-                              bamstats_conf.bed)) {
-                    num_ign_reads += 1;
-                    continue;
-               }
-
-               /* reload ref if necessary */
-               if (target_name == NULL || strcmp(target_name, sam->header->target_name[b->core.tid]) != 0) {
-                    if (ref) {
-                         free(ref);
-                    }
-                    LOG_FIXME("%s\n", "reloading ref");
-                    target_name = sam->header->target_name[b->core.tid];
-                    ref = faidx_fetch_seq(bamstats_conf.fai, target_name,
-                                          0, 0x7fffffff, &ref_len);
-               }
-               if (count_matches(counts, b, ref, bamstats_conf.min_mq)) {
-                    LOG_WARN("%s\n", "count_matches failed on read. ignoring"); /* FIXME print read */
-                    continue;
-               }
-
-               num_good_reads += 1;
-
-               LOG_FIXME("good/ign=%u/%u: m=%d mm=%d i=%d d=%d\n", num_good_reads, num_ign_reads,
-                         counts[MATCH_COUNT_IDX], counts[MISMATCH_COUNT_IDX], counts[INS_COUNT_IDX], counts[DEL_COUNT_IDX]);
-          }
-          if (r < -1) {
-               LOG_FATAL("%s\n", "BAM file is truncated.\n");
-               rc = 1;
-          }
-          bam_destroy1(b);
+          rc = bamstats(sam, &bamstats_conf);
      }
 
-
-     /* cleanup */
-
      samclose(sam);
+
+free_and_exit:
 
      free(outfile);
 
