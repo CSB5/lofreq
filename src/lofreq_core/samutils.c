49adb4111cc1213b2ffc5da65a3a42bab01d6fae
Andreas Wilm <wilma@gis.a-star.edu.sg>
2013-10-01 18:15:58 +0800

    -count_matches() renamed to count_cigar_ops() -Fixed qpos increment in BAM_CSOFT_CLIP
    Former-commit-id: 00626f23cd5447c2da08cb16d5ddcff970a6b165

--- src/lofreq_core/samutils.c
+++ src/lofreq_core/samutils.c
@@ -37,19 +37,28 @@ cigar_str_from_bam(const bam1_t *b)
 /* cigar_str_from_bam() */
 
 
-/* Count matches (OP_MATCH), mismatches (OP_MISMATCH),
- * insertions (OP_INS) and deletions (OP_DEL) for an
- * aligned read. Written to (preallocated, size 4) counts at indices
- * given above. Returns non-0 on error. will ignore all non-del bases
- * if thei bq is below min_bq.
+/* Count matches (OP_MATCH), mismatches (OP_MISMATCH), insertions
+ * (OP_INS) and deletions (OP_DEL) for an aligned read. Written to
+ * (preallocated, size 4) counts at indices given above. Returns non-0
+ * on error. will ignore all non-del bases if their bq is below
+ * min_bq.
  *
- * A slightly more flexible version of this would return an int array
- * of qlen length where each position is set to the corresponding BAM
- * op
+ * If quals is not NULL it will be used as a two dim array (has to be
+ * preallocated) with OPs as first dim (len NUM_OP_CATS) and the
+ * qualities of the bases as second dim. NOTE/FIXME: this uses bq for
+ * mis/matches and INDEL_QUAL_DEFAULT for now in case of indels . The
+ * number of elements corresponds to the count entry and can be at max
+ * readlen.
+ *
+ * Returns the total number of operations counted (excl clips or
+ * bases<mq) or -1 on error
  */
 int
-count_matches(int *counts, const bam1_t *b, const char *ref, int min_bq)
+count_cigar_ops(int *counts, int **quals,
+                const bam1_t *b, const char *ref, int min_bq)
 {
+     const int INDEL_QUAL_DEFAULT = 40;
+     int num_ops = 0;
      /* modelled after bam.c:bam_calend(), bam_format1_core() and
       * pysam's aligned_pairs (./pysam/csamtools.pyx)
       */
@@ -59,16 +68,20 @@ count_matches(int *counts, const bam1_t *b, const char *ref, int min_bq)
      uint32_t qpos = 0; /* pos on read/query */
      uint32_t k, i;
 #ifndef NDEBUG
-     int32_t qlen = (int32_t) bam_cigar2qlen(c, bam1_cigar(b)); /* read length */
+#if 1
+     int32_t qlen = (int32_t) bam_cigar2qlen(c, cigar); /* read length */
+#else
+     int qlen = b->core.l_qseq; /* read length */
+#endif
 #endif
      if (NULL==ref) {
-          return 1;
+          return -1;
      }
 
      assert(NULL != counts);
-
      memset(counts, 0, NUM_OP_CATS*sizeof(int));
-     
+
+    
      /* loop over cigar to get aligned bases
       *
       * read: bam_format1_core(NULL, b, BAM_OFDEC);
@@ -83,24 +96,32 @@ count_matches(int *counts, const bam1_t *b, const char *ref, int min_bq)
            */
           if (op == BAM_CMATCH || op == BAM_CDIFF) {
                for (i=pos; i<pos+l; i++) {                             
+                    int actual_op;
                     assert(qpos < qlen);
                     /* case agnostic */
                     char ref_nt = toupper(ref[i]);
                     char read_nt = bam_nt16_rev_table[bam1_seqi(bam1_seq(b), qpos)];
                     int bq = bam1_qual(b)[qpos];
+#if 0
+                    printf("[M]MATCH qpos,i,ref,read = %d,%d,%c,%c\n", qpos, i, ref_nt, read_nt);
+#endif                    
+
                     if (bq<min_bq) {
                          /* fprintf(stderr, "M: ignoring because of bq=%d at %d (qpos %d)\n", bq, pos, qpos); */
                          qpos += 1;
                          continue;
                     }
-#if 0
-                    printf("[M]MATCH qpos,i,ref,read = %d,%d,%c,%c\n", qpos, i, ref_nt, read_nt);
-#endif                    
+
                     if (ref_nt != read_nt || op == BAM_CDIFF) {
-                         counts[OP_MISMATCH] += 1;
+                         actual_op = OP_MISMATCH;
                     } else {
-                         counts[OP_MATCH] += 1;
+                         actual_op = OP_MATCH;
                     }
+                    counts[actual_op] += 1;
+                    if (quals) {
+                         quals[actual_op][counts[actual_op]-1] = bq;
+                    }
+
                     qpos += 1;
                }
                pos += l;
@@ -118,6 +139,10 @@ count_matches(int *counts, const bam1_t *b, const char *ref, int min_bq)
                     printf("INS qpos,i = %d,None\n", qpos);
 #endif
                     counts[OP_INS] += 1;
+                    if (quals) {
+                         quals[OP_INS][counts[OP_INS]-1] = INDEL_QUAL_DEFAULT; /* FIXME use iq */
+                    }
+
                     qpos += 1;
                }
                
@@ -128,21 +153,36 @@ count_matches(int *counts, const bam1_t *b, const char *ref, int min_bq)
 #endif
                     if (op == BAM_CDEL) {
                         counts[OP_DEL] += 1;
+                        if (quals) {
+                             quals[OP_DEL][counts[OP_DEL]-1] = INDEL_QUAL_DEFAULT; /* FIXME use dq */
+                        }
                     }
                }
                pos += l;
+               /* deletion: don't increase qpos */
 
           } else if (op == BAM_CSOFT_CLIP) {
-               qpos += 1;
+#if 0
+               printf("SOFT CLIP qpos = %d\n", qpos);
+#endif
+               qpos += l;
 
           } else if (op != BAM_CHARD_CLIP) {
-               LOG_WARN("Unknow op %d in cigar %s\n", op, cigar_str_from_bam(b));
+               LOG_WARN("Unknown op %d in cigar %s\n", op, cigar_str_from_bam(b));
+
           }
      } /* for k */
      assert(pos == bam_calend(&b->core, bam1_cigar(b))); /* FIXME correct assert? what if hard clipped? */
+     if (qpos != qlen) {
+               LOG_FIXME("got qpos=%d and qlen=%d for cigar %s l_qseq %d\n", qpos, qlen, cigar_str_from_bam(b), b->core.l_qseq);
+     }
      assert(qpos == qlen); /* FIXME correct assert? What if hard clipped? */
 
-     return 0;
+     num_ops = 0;
+     for (i=0; i<NUM_OP_CATS; i++) {
+          num_ops += counts[i];
+     }
+     return num_ops;
 }
-/* count_matches() */
+/* count_cigar_ops() */
 
