bbbe0f4113466ab7b4bb6d5c9c03a7abff9313d8
Andreas Wilm - CMB5 <wilma@gis.a-star.edu.sg>
2013-10-21 15:10:07 +0800

    Added status message for completed tests

--- tests/filter.sh
+++ tests/filter.sh
@@ -17,6 +17,7 @@ NUMTEST_LIST='100 10000 1000000'
 
 # snv quality with varying alpha
 #
+num_fail=0
 for cor in "bonf" "holm-bonf" "fdr"; do
     last_no=0
     for a in $ALPHA_LIST; do
@@ -25,12 +26,15 @@ for cor in "bonf" "holm-bonf" "fdr"; do
         new_no=$(eval $cmd | grep -c 'snvqual.*\(bonf\|fdr\)') || exit 1
         #echodebug "$cor a=$a: new_no=$new_no last_no=$last_no";# cmd = $cmd"
         if [ $new_no -lt $last_no ]; then
-            echoerror "snvqual: Got fewer SNVs filtered with higher alpha (cmd=$cmd)"
+            echoerror "snvqual: Got fewer SNVs when filtering with higher alpha (cmd=$cmd)"
+            let num_fail=num_fail+1
         fi
         last_no=$new_no
     done
 done
-
+if [ $num_fail -eq 0 ]; then
+    echook "snvqual (var alpha): all tests passed"
+fi
 
 
 # snv quality with varying num_tests
@@ -45,16 +49,20 @@ for cor in "bonf" "holm-bonf" "fdr"; do
         new_no=$(eval $cmd | grep -c 'snvqual.*\(bonf\|fdr\)') || exit 1
         #echodebug "$cor a=$a n=$n: new_no=$new_no last_no=$last_no";# cmd = $cmd"
         if [ $new_no -lt $last_no ]; then
-            echoerror "snvqual: Got fewer SNVs filtered with higher num-tests (cmd=$cmd)"
+            echoerror "snvqual: Got fewer SNVs when filtering with higher num-tests (cmd=$cmd)"
+            let num_fail=num_fail+1
         fi
         last_no=$new_no
     done
 done
-
+if [ $num_fail -eq 0 ]; then
+    echook "snvqual (var num_tests): all tests passed"
+fi
 
 
 # strandbias quality with varying alpha
 #
+num_fail=0
 for cor in "bonf" "holm-bonf"; do
     last_no=100000
     for a in $ALPHA_LIST; do
@@ -63,16 +71,19 @@ for cor in "bonf" "holm-bonf"; do
         new_no=$(eval $cmd | grep -c 'strandbias.*bonf') || exit 1
         #echodebug "$cor a=$a: new_no=$new_no last_no=$last_no";# cmd = $cmd"
         if [ $new_no -gt $last_no ]; then
-            echoerror "strandbias: Got more SNVs filtered with higher alpha (cmd=$cmd)"
+            echoerror "strandbias: Got more SNVs when filtering with higher alpha (cmd=$cmd)"
+            let num_fail=num_fail+1
         fi
         last_no=$new_no
     done
 done
-
-
+if [ $num_fail -eq 0 ]; then
+    echook "strandbias: all tests passed"
+fi
 
 # window filter
 #
+num_fail=0
 vcf=data/vcf/CTTGTA_2_remap_razers-i92_peakrem_corr_nodeff.vcf
 base_cmd="$LOFREQ filter -i $vcf --no-defaults -o -"
 cmd="$base_cmd --window 10"
@@ -80,6 +91,7 @@ num_reg=$(eval $cmd | grep '[^0-9,]85' | grep -c snvwin) || exit 1
 num_exp=4
 if [ $num_reg -ne $num_exp ]; then
     echoerror "window: Got $num_reg but expected $num_exp SNVs (cmd = $cmd)"
+    let num_fail=num_fail+1
 fi
 #
 cmd="$base_cmd --window 1"
@@ -87,5 +99,8 @@ num_reg=$(eval $cmd | grep '[^0-9,]85' | grep -c snvwin) || exit 1
 num_exp=2
 if [ $num_reg -ne $num_exp ]; then
     echoerror "window: Got $num_reg but expected $num_exp SNVs (cmd = $cmd)"
+    let num_fail=num_fail+1
 fi
-
+if [ $num_fail -eq 0 ]; then
+    echook "window: all tests passed"
+fi
