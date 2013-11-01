9ec59dae0ca1d45d8e401fdf80d1c21a1de5f2aa
Andreas Wilm <wilma@gis.a-star.edu.sg>
2013-11-01 15:03:40 +0800

    new functions for error profiling: parse_errprof_statsfile() write_errprof_stats Conditionally compiled if USE_ERRORPROF is true Should consider renaming them to mapping error profile or so

--- src/lofreq_core/samutils.c
+++ src/lofreq_core/samutils.c
@@ -21,6 +21,157 @@
 #include "plp.h"
 #include "samutils.h"
 
+/* libbam:bamaux.c */
+extern void bam_init_header_hash(bam_header_t *header);
+extern void bam_destroy_header_hash(bam_header_t *header);
+
+#define INDEL_QUAL_DEFAULT 40
+
+#define BUF_SIZE 1024
+
+
+typedef struct {
+     int num_targets; /* bam_header->n_targets */
+     int *prop_len; /* one prop length per target: index is tid or target */
+     double **props; /* one prop array per target: index is tid or target */
+} errprof_t;
+
+
+#ifdef USE_ERRORPROF
+int
+parse_errprof_statsfile(FILE *in, bam_header_t *bam_header)
+{
+     char line[BUF_SIZE];
+     int pos = -1;
+     char tname[BUF_SIZE];
+     double prop = -1;
+     errprof_t errprof;
+     int i;
+     int *max_obs_pos;
+     const int default_read_len = 250;
+     int free_bam_header_hash = 0;
+     int rc;
+
+     /* needed for finding tid */
+     if (bam_header->hash == 0) {
+          bam_init_header_hash(bam_header);             
+          free_bam_header_hash = 1;
+     }
+
+     max_obs_pos = calloc(bam_header->n_targets, sizeof(int));
+
+     errprof.num_targets = bam_header->n_targets;
+     errprof.prop_len = calloc(errprof.num_targets, sizeof(int));
+     errprof.props = calloc(errprof.num_targets, sizeof(double *));     
+     for (i=0; i<errprof.num_targets; i++) {
+          errprof.prop_len[i] = default_read_len;/* default alloc here and realloc later */
+          errprof.props[i] = calloc(errprof.prop_len[i], sizeof(double));
+     }
+     i=-1; /* make sure i is not reused improperly; triggers clang warning though */
+
+     while (NULL != fgets(line, BUF_SIZE, in)) {
+         int tid = -1;
+         if (line[0]=='#') {
+             continue;
+         }
+
+         if (3 != sscanf(line, "%s\t%d\t%lg\n", tname, &pos, &prop)) {
+              LOG_ERROR("Couldn't parse line %s\n", line);
+              rc = 1;
+              goto free_and_exit;
+         }
+
+         assert(prop>=0.0 && prop<=1.0);
+
+         pos = pos - 1;
+         assert(pos<MAX_READ_LEN);
+
+         tid = bam_get_tid(bam_header, tname);
+         if (-1 == tid) {
+              LOG_ERROR("Target name '%s' found in error profile doesn't match any of the sequences in BAM header. Skipping and trying to continue...\n", tname);
+              continue;
+         }
+         assert(tid<errprof.num_targets);
+
+         /* for later downsizing */
+         if (pos+1 > max_obs_pos[tid]) {
+              max_obs_pos[tid] = pos+1;
+         }
+
+         /* upsize if necessary */
+         while (pos+1 > errprof.prop_len[tid]) {
+              LOG_ERROR("upsizing pos+1=%d errprof.prop_len[tid=%d]=%d\n\n", pos+1, tid, errprof.prop_len[tid]);
+              errprof.props[tid] = realloc(errprof.props[tid], errprof.prop_len[tid]*2);
+              errprof.prop_len[tid] *= 2;
+         }
+         errprof.props[tid][pos] = prop;
+     }
+
+     /* downsize props */
+     for (i=0; i<errprof.num_targets; i++) {
+          errprof.props[i] = realloc(errprof.props[i], max_obs_pos[i] * sizeof(double));
+          errprof.prop_len[i] = max_obs_pos[i];
+     }
+
+     {/* fixme report */
+          for (i=0; i<errprof.num_targets; i++) {
+               int j;
+               fprintf(stderr, "tid=%d len=%d: ", i, errprof.prop_len[i]);
+               for (j=0; j<errprof.prop_len[i]; j++) {
+                    fprintf(stderr, " %d:%g ", j, errprof.props[i][j]);
+               }
+               fprintf(stderr, "\n");
+          }
+     }
+
+     rc = 0;
+
+free_and_exit:
+
+     /* free errprof */
+     free(errprof.prop_len);
+     for (i=0; i<errprof.num_targets; i++) {
+          free(errprof.props[i]);
+     }
+     free(errprof.props);
+     errprof.num_targets = -1;
+     
+     free(max_obs_pos);
+
+
+     if (free_bam_header_hash) {
+          bam_destroy_header_hash(bam_header);
+     }
+
+     return rc;
+}
+
+
+
+void
+write_errprof_stats(char *target_name, unsigned long int *total_errprof_usedpos, 
+                    double *total_errprof, int max_obs_read_len, FILE *out)
+{
+     /* poor man's version (fw reads only and not taking quality into account):
+      *  samtools view -h -F 0x10 $bam | samtools calmd -S -e - $reffa | cut -f 10 | awk '{for (i=0; i<length($0); i++) {if (substr($0, i, 1)!="=") {c[i]+=1}}} END {for (i in c) {print i, c[i], c[i]/NR}}' | sort -k 1 -n
+      */
+     int i;
+     fprintf(out, "# No-match profile along read after subtracting base-call/indel quality\n");
+     fprintf(out, "# Numbers are in scientific notation\n");
+     fprintf(out, "# chrom\tread-pos\terror-freq\n");
+
+     for (i=0; i<max_obs_read_len; i++) {
+         double prop = 0.0;
+         if (total_errprof_usedpos[i]) {
+              prop = total_errprof[i]/(double)(total_errprof_usedpos[i]);
+         }
+         fprintf(out, "%s\t%d\t%g\n", target_name, i+1, prop);/*, total_errprof[i], total_errprof_usedpos[i]);*/
+     }
+}
+
+#endif
+
+
 
 char *
 cigar_str_from_bam(const bam1_t *b)
@@ -39,6 +190,128 @@ cigar_str_from_bam(const bam1_t *b)
 /* cigar_str_from_bam() */
 
 
+#ifdef USE_ERRORPROF
+
+/* Counts probability of non-match count along the read after
+ * subtracting error prob at that position (using the original
+ * orientation). used_pos is an array of ints indicating whether
+ * position was used or not (trimmed, clipped etc) err_prof and
+ * used_pos must be of at least length b->core.l_qseq.
+ *
+ * WARNING code duplication with count_cigar_ops but merging the two
+ * functions was too complicated
+ */
+void
+calc_read_errprof(double *err_prof, int *used_pos, 
+                   const bam1_t *b, const char *ref)
+{
+     /* modelled after bam.c:bam_calend(), bam_format1_core() and
+      * pysam's aligned_pairs (./pysam/csamtools.pyx)
+      */
+     uint32_t *cigar = bam1_cigar(b);
+     uint32_t k, i;
+     const bam1_core_t *c = &b->core;
+#if 0
+     int32_t qlen = (int32_t) bam_cigar2qlen(c, cigar); /* read length */
+#else
+     int qlen = b->core.l_qseq; /* read length */
+#endif
+     uint32_t pos = c->pos; /* pos on genome */
+     uint32_t qpos = 0; /* pos on read/query */
+     uint32_t qpos_org = bam1_strand(b) ? qlen-qpos-1 : qpos;/* original qpos before mapping as possible reverse */
+
+     memset(err_prof, 0, b->core.l_qseq * sizeof(double));
+     memset(used_pos, 0, b->core.l_qseq * sizeof(int));
+
+     /* loop over cigar to get aligned bases
+      *
+      * read: bam_format1_core(NULL, b, BAM_OFDEC);
+      */
+     for (k=0; k < c->n_cigar; ++k) { /* n_cigar: number of cigar operations */
+          int op = cigar[k] & BAM_CIGAR_MASK; /* the cigar operation */
+          uint32_t l = cigar[k] >> BAM_CIGAR_SHIFT;
+
+          /* following conditionals could be collapsed to much shorter
+           * code, but we keep them as they were in pysam's
+           * aligned_pairs to make later handling of indels easier
+           */
+          if (op == BAM_CMATCH || op == BAM_CDIFF) {
+               for (i=pos; i<pos+l; i++) {                             
+                    assert(qpos < qlen);
+                    /* case agnostic */
+                    char ref_nt = toupper(ref[i]);
+                    char read_nt = bam_nt16_rev_table[bam1_seqi(bam1_seq(b), qpos)];
+                    int bq = bam1_qual(b)[qpos];
+#if 0
+                    printf("[M]MATCH qpos,i,ref,read = %d,%d,%c,%c\n", qpos, i, ref_nt, read_nt);
+#endif                    
+
+                    if (ref_nt != read_nt || op == BAM_CDIFF) {
+                         err_prof[qpos_org] = 1.0 - PHREDQUAL_TO_PROB(bq);
+                    } /* otherwise leave at 0.0 but count anyway */
+                    used_pos[qpos_org] = 1;
+
+                    qpos += 1;
+                    qpos_org = bam1_strand(b) ? qlen-qpos-1 : qpos;
+               }
+               pos += l;
+
+          } else if (op == BAM_CINS) {
+               for (i=pos; i<pos+l; i++) {
+                    assert(qpos < qlen);
+                    
+                    err_prof[qpos] = 1.0 - PHREDQUAL_TO_PROB(INDEL_QUAL_DEFAULT);
+                    used_pos[qpos] = 1;
+#if 0
+                    printf("INS qpos,i = %d,None\n", qpos);
+#endif
+                    qpos += 1;
+                    qpos_org = bam1_strand(b) ? qlen-qpos-1 : qpos;
+               }
+               
+          } else if (op == BAM_CDEL || op == BAM_CREF_SKIP) {
+               for (i=pos; i<pos+l; i++) {
+#if 0
+                    printf("DEL qpos,i = None,%d\n", i);
+#endif
+
+                    if (op == BAM_CDEL) {
+                         err_prof[qpos] = 1.0 - PHREDQUAL_TO_PROB(INDEL_QUAL_DEFAULT);
+                         used_pos[qpos] = 1;
+                    }
+               }
+               pos += l;
+               /* deletion: don't increase qpos */
+
+          } else if (op == BAM_CSOFT_CLIP) {
+#if 0
+               printf("SOFT CLIP qpos = %d\n", qpos);
+#endif
+               qpos += l;
+               qpos_org = bam1_strand(b) ? qlen-qpos-1 : qpos;
+
+          } else if (op != BAM_CHARD_CLIP) {
+               LOG_WARN("Unknown op %d in cigar %s\n", op, cigar_str_from_bam(b));
+
+          }
+     } /* for k */
+     assert(pos == bam_calend(&b->core, bam1_cigar(b))); /* FIXME correct assert? what if hard clipped? */
+     if (qpos != qlen) {
+          LOG_FIXME("got qpos=%d and qlen=%d for cigar %s l_qseq %d\n", qpos, qlen, cigar_str_from_bam(b), b->core.l_qseq);
+     }
+     assert(qpos == qlen); /* FIXME correct assert? What if hard clipped? */
+
+#if 0
+     fprintf(stderr, "%s:", __FUNCTION__);
+     for (i=0; i< b->core.l_qseq; i++) {
+          fprintf(stderr, " %g/%d", err_prof[i], used_pos[i]);
+     }
+     fprintf(stderr, "\n");
+#endif
+}
+#endif
+
+
 /* Count matches (OP_MATCH), mismatches (OP_MISMATCH), insertions
  * (OP_INS) and deletions (OP_DEL) for an aligned read. Written to
  * (preallocated, size 4) counts at indices given above. Returns non-0
@@ -51,19 +324,21 @@ cigar_str_from_bam(const bam1_t *b)
  * mis/matches and INDEL_QUAL_DEFAULT for now in case of indels . The
  * number of elements corresponds to the count entry and can be at max
  * readlen.
- *
+ * 
  * If target is non-NULL will ignore preloaded variant positions via
  * var_in_ign_list
  * 
  * Returns the total number of operations counted (excl clips or
  * bases<mq) or -1 on error
+ *
+ * WARNING code duplication with count_cigar_ops but merging the two
+ * functions was too complicated
  */
 int
 count_cigar_ops(int *counts, int **quals,
                 const bam1_t *b, const char *ref, int min_bq,
                 char *target)
 {
-     const int INDEL_QUAL_DEFAULT = 40;
      int num_ops = 0;
      /* modelled after bam.c:bam_calend(), bam_format1_core() and
       * pysam's aligned_pairs (./pysam/csamtools.pyx)
@@ -73,7 +348,7 @@ count_cigar_ops(int *counts, int **quals,
      uint32_t pos = c->pos; /* pos on genome */
      uint32_t qpos = 0; /* pos on read/query */
      uint32_t k, i;
-#if 1
+#if 0
      int32_t qlen = (int32_t) bam_cigar2qlen(c, cigar); /* read length */
 #else
      int qlen = b->core.l_qseq; /* read length */
@@ -133,7 +408,6 @@ count_cigar_ops(int *counts, int **quals,
                          actual_op = OP_MATCH;
                     }
 
- 
                     counts[actual_op] += 1;
                     if (quals) {
                          quals[actual_op][counts[actual_op]-1] = bq;
@@ -186,7 +460,7 @@ count_cigar_ops(int *counts, int **quals,
                          if (target) {
                               var_t fake_var;
                               fake_var.chrom = target;
-                              fake_var.pos = i; /* FIXME does i that make sense here, i.e. matches dbSNP entry? */
+                              fake_var.pos = i; /* FIXME does i make sense here, i.e. matches dbSNP entry? */
                               if (var_in_ign_list(&fake_var)) {
                                    continue;
                               }
