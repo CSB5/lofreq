19c5fd37c7f61667c0ff34d45071a26cd37615fb
Andreas Wilm <wilma@gis.a-star.edu.sg>
2013-11-05 10:42:44 +0800

    Freeing bam_header again after temp disabling it due to segfaults (caused somewhere else)
    Former-commit-id: 8cda3f5c51eccd089aba1ed0f91c3d6b43d08f62

--- src/lofreq_core/samutils.c
+++ src/lofreq_core/samutils.c
@@ -116,7 +116,7 @@ parse_alnerrprof_statsfile(alnerrprof_t *alnerrprof, const char *path, bam_heade
      /* needed for finding tid from tname */
      if (bam_header->hash == 0) {
           bam_init_header_hash(bam_header);             
-          free_bam_header_hash = 0; /* FIXME sometimes segfaults for unknown reasons */
+          free_bam_header_hash = 1;
      }
 
      max_obs_pos = calloc(bam_header->n_targets, sizeof(int));
