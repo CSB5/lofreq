2cab608ec91c2d159377ee850138c5cfaba0ed5f
Andreas Wilm <wilma@gis.a-star.edu.sg>
2013-12-06 14:43:54 +0800

    Initial commit

---
+++ src/lofreq/lofreq_index.c
@@ -0,0 +1,41 @@
+/* -*- c-file-style: "k&r"; indent-tabs-mode: nil; -*- */
+
+/*********************************************************************
+ *
+ * FIXME update license
+ *
+ *********************************************************************/
+
+/* This is an almost one to one copy of the corresponding bits in
+ * samtools' bam_index.c */
+
+#include <ctype.h>
+#include <assert.h>
+
+/* samtools includes */
+#include "bam.h"
+int bam_index(int argc, char *argv[]);
+int bam_idxstats(int argc, char *argv[]);
+
+/* lofreq includes */
+#include "log.h"
+
+
+#if 1
+#define MYNAME "lofreq"
+#else
+#define MYNAME PACKAGE
+#endif
+
+
+int
+main_index(int argc, char *argv[])
+{
+    return bam_index(argc-1, argv+1);
+}
+
+int
+main_idxstats(int argc, char *argv[])
+{
+    return bam_idxstats(argc-1, argv+1);
+}
