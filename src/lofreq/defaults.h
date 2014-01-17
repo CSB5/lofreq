b50392894b6251d7f50dba273b0ffa84ebea3a44
Andreas Wilm <wilma@gis.a-star.edu.sg>
2014-01-17 09:22:16 +0800

    Introduction of DEFAULT_MIN_ALT_MERGEDQ

--- src/lofreq/defaults.h
+++ src/lofreq/defaults.h
@@ -1,15 +1,35 @@
 #ifndef DEFAULTS_H
 #define DEFAULTS_H
 
+/*
+ * This file contains default settings of values that can be set by user on command line 
+ *
+ */
+
+
+/* mapping quality filters: applied to all reads. don't set too high as
+ * this is a mapper dependent value
+ * in case of BWA it's also dependent on the alignment command used.
+ */
 #define DEFAULT_MIN_MQ 13
 #define DEFAULT_MAX_MQ 255
-#define DEFAULT_MIN_BQ 3
-#define DEFAULT_MIN_ALT_BQ 13
-#define DEFAULT_DEF_ALT_BQ -1
-#define DEFAULT_MIN_COV 1
-#define DEFAULT_DEF_NM_QUAL 13
-#define DEFAULT_MAX_PLP_DEPTH 1000000
-#define DEFAULT_BAQ_ON 1
 
+/* minimum base quality of any base. note: GATK doesn't recalibrate BQ <=5 */
+#define DEFAULT_MIN_BQ 6
+
+/* minimum base quality for alt bases */
+#define DEFAULT_MIN_ALT_BQ 20
+#define DEFAULT_DEF_ALT_BQ -1
+/* FIXME make arg FIXME might affect on src qual untested */
+#define DEFAULT_MIN_ALT_MERGEDQ 20
+
+/* non match quality for source qual */
+#define DEFAULT_DEF_NM_QUAL 20
+ 
+/* coverage thresholds */
+#define DEFAULT_MIN_COV 1
+#define DEFAULT_MAX_PLP_DEPTH 1000000
+
+#define DEFAULT_BAQ_ON 1
 
 #endif
