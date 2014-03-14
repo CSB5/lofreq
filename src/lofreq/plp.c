26f878cd02f081d2fefa2998a67758b556413bc6
Andreas Wilm - CMB5 <wilma@gis.a-star.edu.sg>
2014-03-14 10:54:17 +0800

    Now saving more memory by only keeping key and not adding var to ign_hash
    Former-commit-id: 62c8afada853f361021166d1e684a9bfba2411be

--- src/lofreq/plp.c
+++ src/lofreq/plp.c
@@ -310,7 +310,6 @@ source_qual_load_ign_vcf(const char *vcf_path, void *bed)
          char *key;
          int rc;
          var_hash_t *match = NULL;
-         int i;
 
          vcf_new_var(&var);
          rc = vcf_parse_var(& vcf_file, var);
@@ -347,18 +346,10 @@ source_qual_load_ign_vcf(const char *vcf_path, void *bed)
 #ifdef TRACE
          LOG_DEBUG("Adding %s\n", key);
 #endif
-         /* since we only need the key and no other info we can delete
-          * a lot of var members to save memory. simplest and with
-          * biggest effect is info. format and samples are further
-          * candidates */
-         free(var->info); var->info = NULL;
-         free(var->format); var->format = NULL;
-         for (i=0; i<var->num_samples; i++) {
-              free(var->samples[i]);
-         }
-         free(var->samples); var->samples = NULL;
-
-         var_hash_add(& source_qual_ign_vars_hash, key, var);
+         /* since we only need the key and no other info we do
+          * not need to save the var (and save NULL instead) */
+         vcf_free_var(&var);
+         var_hash_add(& source_qual_ign_vars_hash, key, NULL);
     }
 
     num_kept_vars = HASH_COUNT(source_qual_ign_vars_hash);
