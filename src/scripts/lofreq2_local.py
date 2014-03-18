1be606bfb97eaad3a4d3494aba92d434f012e47b
Andreas Wilm <wilma@gis.a-star.edu.sg>
2014-03-18 22:31:46 +0800

    Now got two versions of lofreq2_local.py, one for scripts one for tools
    Former-commit-id: 16ed9c694551d1cc1e14a0c823a7bed13cd8cf6f

--- src/scripts/lofreq2_local.py
+++ src/scripts/lofreq2_local.py
@@ -13,19 +13,19 @@ if os.path.exists(os.path.join(d, "lofreq_star")):
     sys.path.insert(0, d)
 
 # Set PATH such that we find lofreq binary first
-#d = os.path.normpath(os.path.join(
-#    os.path.dirname(sys.argv[0]), '../../lofreq'))
-#if os.path.exists(os.path.join(d, 'lofreq')):
-#    #sys.stderr.write("NOTE: Adding local dir %s to PATH\n" % d)
-#    os.environ["PATH"] = d + os.pathsep + os.environ["PATH"]
+d = os.path.normpath(os.path.join(
+    os.path.dirname(sys.argv[0]), '../../lofreq'))
+if os.path.exists(os.path.join(d, 'lofreq')):
+    #sys.stderr.write("NOTE: Adding local dir %s to PATH\n" % d)
+    os.environ["PATH"] = d + os.pathsep + os.environ["PATH"]
 
 # In theory need to find scripts because the main binary knows about them. However, there are circular cases where script call the binary which then can't find the scripts again (e.g. in parallel wrapper),so:
 #
-#d = os.path.normpath(os.path.join(
-#    os.path.dirname(sys.argv[0]), '../tools/scripts'))
-#if os.path.exists(d):
-#    #sys.stderr.write("NOTE: Adding local dir %s to PATH\n" % d)
-#    os.environ["PATH"] = d + os.pathsep + os.environ["PATH"]
+d = os.path.normpath(os.path.join(
+    os.path.dirname(sys.argv[0]), '../scripts'))
+if os.path.exists(d):
+    #sys.stderr.write("NOTE: Adding local dir %s to PATH\n" % d)
+    os.environ["PATH"] = d + os.pathsep + os.environ["PATH"]
     
 
         
