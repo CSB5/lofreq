452db08dc22edd427d6ee0ab71346bb7bf378a59
Andreas Wilm <wilma@gis.a-star.edu.sg>
2014-03-19 09:03:18 +0800

    Initial commit
    Former-commit-id: f34e11675dbacc5f33a79d285cba62645344fff1

---
+++ binary_installer.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+set -o pipefail
+
+PREFIX=/usr/local
+
+usage() {
+    # keep in sync with arg parsing below
+cat <<EOF
+$(basename $0): binary installer for LoFreq
+
+  Options:
+    -p | --prefix  : Install to this directory (default: $PREFIX)
+    -h | --help    : Display this help
+EOF
+}
+
+while [ "$1" != "" ]; do
+    case $1 in
+        -p | --prefix )
+            shift
+            prefix=$1
+            ;;
+        -h | --help )
+            usage
+            exit
+            ;;
+        * ) 
+            echo "FATAL: unknown argument \"$1\""
+            usage
+            exit 1
+    esac
+    shift
+done
+
+test -z "$prefix" && prefix=$PREFIX
+echo "Using $prefix as installation prefix"
+
+echo "Installing binaries"
+test -d "$prefix/bin" || mkdir -p $prefix/bin || exit 1
+for f in ./src/lofreq/lofreq src/scripts/lofreq2_call_pparallel.py src/scripts/lofreq2_somatic.py; do
+    if [ ! -s $f ]; then
+        echo "FATAL: can't find $f" 1>&2
+        exit 1
+    fi
+    cp $f $prefix/bin/ || exit 1
+done
+
+echo "Installing Python tools"
+pushd ./src/tools >/dev/null
+pythonpath=$(python setup.py install --prefix $prefix | \
+    grep 'egg-info$' | head -n1 | cut -f 2 -d ' ' | sed -e 's,LoFreq.*,,') || exit 1
+
+echo "NOTE: Make sure $pythonpath is in your PYTHONPATH"
+popd >/dev/null
+
+echo "Successful exit"
