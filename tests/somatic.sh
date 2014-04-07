65d96695cc4e9ed67bfce6bcb3f918add646cd61
Andreas Wilm - CMB5 <wilma@gis.a-star.edu.sg>
2014-04-07 15:48:10 +0800

    Do never use fa.gz!

--- tests/somatic.sh
+++ tests/somatic.sh
@@ -10,7 +10,7 @@ KEEP_TMP=0
 bam_n=./data/somatic/CHH966-normal-100x-100pur-hg19.chr22-bed-only.bam
 bam_t=./data/somatic/CHH966-tumor-100x-10pur-hg19.chr22-bed-only.bam
 bed=./data/somatic/SeqCap_EZ_Exome_v3_primary_lib_extend_no_overlap_minus300.chr22.bed
-reffa=./data/somatic/hg19_chr22.fa.gz
+reffa=./data/somatic/hg19_chr22.fa
 truesnv=./data/somatic/hg19_chr22_true_snv.vcf.gz
 outprefix=$(mktemp -t $(basename $0) 2>/dev/null || mktemp -t $(basename $0).XXXXXX);#XXXXXX needed on linux?
 if [ $KEEP_TMP -eq 1 ]; then
