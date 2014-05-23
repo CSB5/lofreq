e886875921471917ef3b52c1e456ee0b2e345651
Andreas Wilm - CMB5 <wilma@gis.a-star.edu.sg>
2014-05-23 13:56:22 +0800

    Initial commit
    Former-commit-id: 71e0d176480b7c2cd3e120b3d346252a0c3197f3

---
+++ tests/valgrind_vcfset.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# FIXME:add-doc
+
+source lib.sh || exit 1
+
+valgrind_log=$(mktemp -t $(basename $0).XXXXXX.valgrind)
+vcf_in=data/vcf/CTTGTA_2_remap_razers-i92_peakrem_corr_nodeff.vcf.gz
+
+valgrind --log-file=$valgrind_log --tool=memcheck $LOFREQ vcfset -a complement -1 $vcf_in -2 $vcf_in >/dev/null || exit 1
+#valgrind --log-file=$valgrind_log --tool=memcheck $lofreq vcfset -a intersect -1 $vcf_in -2 $vcf_in >/dev/null || exit 1
+
+test -s $valgrind_log || exit 1
+
+num_err=$(grep 'ERROR SUMMARY' $valgrind_log | grep -cv ': 0 errors')
+if [ "$num_err" -ne 0 ]; then
+    echoerror "Found errors in Valgrind output $valgrind_log"
+    exit 1
+else
+    echook "No errors found in Valgrind output"
+fi
+
+lost_bytes=$(grep 'lost' $valgrind_log | grep -cv ': 0 bytes in 0 blocks')
+if [ "$lost_bytes" -ne 0 ]; then
+    echoerror "Found lost bytes in Valgrind output $valgrind_log" || exit 1
+    exit 1
+else
+    echook "No lost bytes found in Valgrind output"
+fi
+
