3592e24464aab0b5c00bb1e0c627071759be5196
Andreas Wilm - CMB5 <wilma@gis.a-star.edu.sg>
2014-08-21 16:10:04 +0800

    deleted old tests

--- tests.old/internal_sens.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-lofreq_snpcaller.py --test-sens || exit 1
-echo "OK"
-
-
-
- 
--- tests.old/run_all.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for f in $(ls *sh | grep -v run_all.sh | grep -v lib.sh); do
-	echo "*** Running $f";
-	./$f || echo "FAILED: $f" ;
-	echo;
-done
