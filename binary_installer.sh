b54eb1bd82ef697d2612e2a138402ae626bde223
Andreas Wilm - CMB5 <wilma@gis.a-star.edu.sg>
2014-10-23 10:51:29 +0800

    Added lofreq2_alnqual to binary_installer.sh

--- binary_installer.sh
+++ binary_installer.sh
@@ -38,7 +38,8 @@ echo "Using $prefix as installation prefix"
 
 echo "Installing binaries"
 test -d "$prefix/bin" || mkdir -p $prefix/bin || exit 1
-for f in ./src/lofreq/lofreq src/scripts/lofreq2_call_pparallel.py src/scripts/lofreq2_somatic.py; do
+BINARIES="./src/lofreq/lofreq src/scripts/lofreq2_call_pparallel.py src/scripts/lofreq2_somatic.py src/lofreq_alnqual/lofreq2_alnqual"
+for f in $BINARIES; do
     if [ ! -s $f ]; then
         echo "FATAL: can't find $f" 1>&2
         exit 1
