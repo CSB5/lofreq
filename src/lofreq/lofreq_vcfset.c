9136cfddd8f826119a63d70ab88ee4d1764b441c
Andreas Wilm - CMB5 <wilma@gis.a-star.edu.sg>
2014-11-04 21:25:36 +0800

    vcfset: implemented --add-info and -a concat

--- src/lofreq/lofreq_vcfset.c
+++ src/lofreq/lofreq_vcfset.c
@@ -46,7 +46,7 @@ typedef enum {
      SETOP_UNKNOWN,
      SETOP_INTERSECT,
      SETOP_COMPLEMENT, 
-     SETOP_UNION
+     SETOP_CONCAT
 } vcfset_op_t;
 
 typedef struct {
@@ -69,15 +69,15 @@ usage(const vcfset_conf_t* vcfset_conf)
      fprintf(stderr, "Usage: %s [options] -a op -1 1.vcf -2 2.vcf \n", MYNAME);
 
      fprintf(stderr,"Options:\n");
-     fprintf(stderr, "  -1 | --vcf1 FILE      1st VCF input file (gzip supported)\n");
-     fprintf(stderr, "  -2 | --vcf2 FILE      2nd VCF input file (gzip supported)\n");
+     fprintf(stderr, "  -1 | --vcf1 FILE      1st VCF input file (bgzip supported)\n");
+     fprintf(stderr, "  -2 | --vcf2 FILE      2nd VCF input file (mandatory - except for concat - and needs to be tabix indexed)\n");
      fprintf(stderr, "  -o | --vcfout         VCF output file (default: - for stdout; gzip supported).\n"
              "                        Meta-data will be copied from vcf1\n");
-     fprintf(stderr, "  -a | --action         Set operation to perform:\n"
-             "                        intersect or complement.\n"
-             "                        intersect = vcf1 AND vcf2.\n"
-/*             "                        union = vcf1 OR vcf2.\n"*/
-             "                        complement = vcf1 \\ vcf2.\n");
+     fprintf(stderr, "  -a | --action         Set operation to perform: intersect, complement or concat.\n"
+             "                        - intersect = vcf1 AND vcf2.\n"
+             "                        - complement = vcf1 \\ vcf2.\n"
+             "                        - concat = vcf1 + vcf2 ... vcfn (order as in input!)\n");
+     fprintf(stderr, "  -I | --add-info STR   Add info field, e.g. 'SOMATIC'\n");
      fprintf(stderr, "       --count-only     Don't print bases, just numbers\n");
      fprintf(stderr, "       --only-pos       Disable allele-awareness by using position only (ignoring bases) as key for storing and comparison\n");
      fprintf(stderr, "       --only-passed    Ignore variants marked as filtered\n");
@@ -98,9 +98,8 @@ main_vcfset(int argc, char *argv[])
      char *vcf_header = NULL;
      int rc = 0;
      char *vcf_in1, *vcf_in2, *vcf_out;
-     long int num_vars_vcf1, num_vars_vcf2;
-     long int num_vars_vcf1_ign, num_vars_vcf2_ign, num_vars_out;
-     var_hash_t *var_hash_vcf2 = NULL; /* must be declared NULL ! */
+     long int num_vars_vcf1;
+     long int num_vars_vcf1_ign, num_vars_out;
      static int only_passed = 0;
      static int only_pos = 0;
      static int only_snvs = 0;
@@ -108,10 +107,11 @@ main_vcfset(int argc, char *argv[])
      static int count_only = 0;
      tbx_t *vcf2_tbx = NULL; /* index for second vcf file */
      htsFile *vcf2_hts = NULL;
-
+     char *add_info_field = NULL;
+     int vcf_concat_findex = 0;
      vcf_in1 = vcf_in2 = vcf_out = NULL;
-     num_vars_vcf1 = num_vars_vcf2 = 0;
-     num_vars_vcf1_ign = num_vars_vcf2_ign = num_vars_out = 0;
+     num_vars_vcf1 = 0;
+     num_vars_vcf1_ign = num_vars_out = 0;
 
      /* default vcfset options */
      memset(&vcfset_conf, 0, sizeof(vcfset_conf_t));
@@ -142,12 +142,13 @@ main_vcfset(int argc, char *argv[])
               {"vcf2", required_argument, NULL, '2'},
               {"vcfout", required_argument, NULL, 'o'},
               {"action", required_argument, NULL, 'a'},
+              {"add-info", required_argument, NULL, 'I'},
 
               {0, 0, 0, 0} /* sentinel */
          };
 
          /* keep in sync with long_opts and usage */
-         static const char *long_opts_str = "h1:2:o:a:";
+         static const char *long_opts_str = "h1:2:o:a:I:";
 
          /* getopt_long stores the option index here. */
          int long_opts_index = 0;
@@ -190,14 +191,18 @@ main_vcfset(int argc, char *argv[])
               } else if (0 == strcmp(optarg, "complement")) {
                    vcfset_conf.vcf_setop = SETOP_COMPLEMENT;
 
-/*              } else if (0 == strcmp(optarg, "union")) {
-                vcfset_conf.vcf_setop = SETOP_UNION; */
+              } else if (0 == strcmp(optarg, "concat")) {
+                   vcfset_conf.vcf_setop = SETOP_CONCAT;
 
               } else {
                    LOG_FATAL("Unknown action '%s'. Exiting...\n", optarg);
                    free(vcf_in1); free(vcf_in2); free(vcf_out);
                    return 1;
               }
+              break;
+
+         case 'I': 
+              add_info_field = strdup(optarg);
               break;
 
          case '?': 
@@ -220,11 +225,24 @@ main_vcfset(int argc, char *argv[])
          return 1;
     }
 
-
     if (0 != argc - optind - 1) {
-         LOG_FATAL("%s\n", "Unrecognized arguments found\n");
-         return 1;
+         if (vcfset_conf.vcf_setop == SETOP_CONCAT) {
+              vcf_concat_findex = optind;
+         } else {
+              LOG_FATAL("%s\n", "Unrecognized arguments found\n");
+              return 1;
+         }
+    } else {
+         if (vcfset_conf.vcf_setop == SETOP_CONCAT) {
+              LOG_FATAL("%s\n", "No extra files for concat given\n");
+              return 1;
+         }
     }
+#if 0
+    int i; for (i=optind+1; i<argc; i++) {
+         LOG_FIXME("argv[%d]=%s\n", i, argv[i]);
+    }
+#endif
 
     if (argc == 2) {
         fprintf(stderr, "\n");
@@ -240,13 +258,17 @@ main_vcfset(int argc, char *argv[])
          return 1;
     }
 
-    /* all input files must be specified */
-    if  (vcf_in1 == NULL || vcf_in2 == NULL) {
+    if  (vcf_in1 == NULL || (vcf_in2 == NULL && vcfset_conf.vcf_setop != SETOP_CONCAT)) {
          LOG_FATAL("%s\n\n", "At least one vcf input file not specified");
          usage(& vcfset_conf);
          free(vcf_in1); free(vcf_in2); free(vcf_out);
          return 1;
-         
+    }
+    if (vcf_in2 != NULL && vcfset_conf.vcf_setop == SETOP_CONCAT) {
+         LOG_FATAL("%s\n\n", "For concat just use the -1 option followed by all other vcf files instead of using -2");
+         usage(& vcfset_conf);
+         free(vcf_in1); free(vcf_in2); free(vcf_out);
+         return 1;         
     }
 
     if (vcf_file_open(& vcfset_conf.vcf_in1, vcf_in1, 
@@ -256,18 +278,18 @@ main_vcfset(int argc, char *argv[])
          return 1;
     }
 
-
-    vcf2_hts = hts_open(vcf_in2, "r");
-    if (!vcf2_hts) {
-         LOG_FATAL("Couldn't load %s\n", vcf_in2);
-         return 1;
+    if (vcf_in2) {
+         vcf2_hts = hts_open(vcf_in2, "r");
+         if (!vcf2_hts) {
+              LOG_FATAL("Couldn't load %s\n", vcf_in2);
+              return 1;
+         }
+         vcf2_tbx = tbx_index_load(vcf_in2);
+         if (!vcf2_tbx) {
+              LOG_FATAL("Couldn't load tabix index for %s\n", vcf_in2);
+              return 1;
+         }
     }
-    vcf2_tbx = tbx_index_load(vcf_in2);
-    if (!vcf2_tbx) {
-         LOG_FATAL("Couldn't load tabix index for %s\n", vcf_in2);
-         return 1;
-    }
-
 
     /* vcf_out default if not set: stdout==- */
     if (! vcf_out) {
@@ -283,11 +305,6 @@ main_vcfset(int argc, char *argv[])
               return 1;
          }
     }
-
-    free(vcf_in1);
-    free(vcf_in2);
-    free(vcf_out);
-
 
     /* use meta-data/header of vcf_in1 for output
      */
@@ -326,7 +343,32 @@ main_vcfset(int argc, char *argv[])
          }
          if (1 == rc) {/* EOF */
               free(var1);
-              break;
+              
+              if (vcfset_conf.vcf_setop != SETOP_CONCAT) {
+                   break;
+              } else {
+                   vcf_concat_findex++;
+                   if (vcf_concat_findex==argc) {
+                        break;
+                   }
+                   /* set vcf1 up anew and simply continue as if nothing happened 
+                    */
+                   vcf_file_close(& vcfset_conf.vcf_in1);
+                   free(vcf_in1);
+
+                   vcf_in1 = strdup(argv[vcf_concat_findex]);
+                   LOG_DEBUG("updated vcf_in1 = %s\n", vcf_in1);
+                   if (vcf_file_open(& vcfset_conf.vcf_in1, vcf_in1, 
+                                     HAS_GZIP_EXT(vcf_in1), 'r')) {
+                        LOG_ERROR("Couldn't open %s\n", vcf_in1);
+                        free(vcf_in1); free(vcf_in2); free(vcf_out);
+                        return 1;
+                   }
+                   if (0 != vcf_skip_header(& vcfset_conf.vcf_in1)) {
+                        LOG_WARN("skip header failed for %s\n", vcf_in1);
+                   }
+                   continue;
+              }
          }
 
          is_indel = vcf_var_is_indel(var1);
@@ -347,11 +389,25 @@ main_vcfset(int argc, char *argv[])
               vcf_free_var(& var1);
               continue;
          }
+         if (add_info_field) {
+              vcf_var_add_to_info(var1, add_info_field);
+         }
+         num_vars_vcf1 += 1;
 #ifdef TRACE
          fprintf(stderr, "var1 pass: "); vcf_write_var(stderr, var1);
 #endif
-         num_vars_vcf1 += 1;
 
+         if (vcfset_conf.vcf_setop == SETOP_CONCAT) {
+              num_vars_out += 1;
+              if (! count_only) {
+                   vcf_write_var(& vcfset_conf.vcf_out, var1);
+              }
+              vcf_free_var(& var1);
+              /* skip comparison against vcf2 */
+              continue;
+         }
+
+         /* use index access to vcf2 */
          snprintf(regbuf, 1024, "%s:%ld-%ld", var1->chrom, var1->pos+1, var1->pos+1);
          var2_itr = tbx_itr_querys(vcf2_tbx, regbuf);
          if (! var2_itr) {
@@ -381,6 +437,7 @@ main_vcfset(int argc, char *argv[])
                    }
               }
          }
+
          if (vcfset_conf.vcf_setop == SETOP_COMPLEMENT) {
               /* relative complement : elements in A but not B */
               if (!var2_match) {
@@ -404,11 +461,13 @@ main_vcfset(int argc, char *argv[])
 
          vcf_free_var(& var1);
          tbx_itr_destroy(var2_itr);
-    }
-    vcf_file_close(& vcfset_conf.vcf_in1);
-    hts_close(vcf2_hts);
-    tbx_destroy(vcf2_tbx);
+    }/* while (1) */
 
+    vcf_file_close(& vcfset_conf.vcf_in1);
+    if (vcf_in2) {
+         hts_close(vcf2_hts);
+         tbx_destroy(vcf2_tbx);
+    }
     LOG_VERBOSE("Parsed %d variants from 1st vcf file (ignoring %d non-passed of those)\n", 
                 num_vars_vcf1 + num_vars_vcf1_ign, num_vars_vcf1_ign);
     LOG_VERBOSE("Wrote %d variants to output\n", 
@@ -416,7 +475,6 @@ main_vcfset(int argc, char *argv[])
     if (! count_only) {
          vcf_file_close(& vcfset_conf.vcf_out);
     }
-    var_hash_free_table(var_hash_vcf2);
 
     if (0==rc) {
          if (count_only) {
@@ -425,6 +483,11 @@ main_vcfset(int argc, char *argv[])
 
          LOG_VERBOSE("%s\n", "Successful exit.");
     }
+
+    free(vcf_in1);
+    free(vcf_in2);
+    free(vcf_out);
+
 
     return rc;
 }
