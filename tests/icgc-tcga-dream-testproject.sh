e6dfc101e67129905f94cedd5f82e09b89b62898
Andreas Wilm - CMB5 <wilma@gis.a-star.edu.sg>
2015-01-23 17:32:03 +0800

    icgc-tcga-dream-testproject.sh: Added tmpdir handling and changed code to be comparable to icgc-tcga-dream-indel_chr19.sh

--- tests/icgc-tcga-dream-testproject.sh
+++ tests/icgc-tcga-dream-testproject.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 source lib.sh || exit 1
 
+KEEP_TMP=0
 BASE=data/icgc-tcga-dream-testproject/
 #BASE=/projects/wilma/SOMATIC/dream-challenge/testproject/
 #BASE=/mnt/userArchive/wilma/projects/somatic/testproject/
@@ -12,6 +13,9 @@ BED=${BASE}/chr20.bed
 #EVALUATOR=/projects/wilma/SOMATIC/dream-challenge/tools/bamsurgeon.git/etc/evaluator.py
 EVALUATOR=data/icgc-tcga-dream-support/evaluator.py
 DEBUG=0
+
+# threads=16; echoinfo "overwriting default threads to $threads"
+
 
 for f in $REF $TUMOR $NORMAL $TRUTH $EVALUATOR; do
     if [ ! -s $f ]; then
@@ -33,6 +37,7 @@ else
     echoinfo "lofreq somatic run completed. now checking results"
 fi
 
+num_err=0
 # use bamsurgeon evaluator
 #
 # example output
@@ -40,8 +45,23 @@ fi
 # tpcount, fpcount, subrecs, trurecs:
 # 1389 15 1404 1445
 # precision, recall, F1 score: 0.989316239316,0.96124567474,0.975078975079
-res=$($EVALUATOR -t $TRUTH -v $vcf_out -m SNV | awk 'END {print $NF}') || exit
-echo $res | awk -F, '{prec=$1; rec=$2; if (prec<0.98 || rec<0.96) {status="ERROR"} else {status="OK"} printf "%s: precision=%f recall=%f\n", status, prec, rec > "/dev/stderr"}'
+title="snvs before dbsnp removal"
+res_ll=$($EVALUATOR -t $TRUTH -v $vcf_out -m SNV | awk 'END {print $NF}') || exit
 
-#rm $vcf_out
+res=$(echo $res_ll | \
+    awk -F, '{prec=$1; rec=$2; if (prec<0.98 || rec<0.96) {status="ERROR"} else {status="OK"} printf "%s: precision=%f recall=%f\n", status, prec, rec}') || exit 1
+if echo $res | grep -q ERROR; then
+   let num_err=num_err+1
+fi
+echo "$title: "$res 1>&2
+
+
+if [ $KEEP_TMP -ne 1 ] && [ $num_err -eq 0 ]; then
+    test -d $outdir && rm -rf $outdir
+else
+    echowarn "Not deleting temporary output directory $outdir"
+fi
+if [ $num_err -ne 0 ]; then
+    exit 1
+fi
 
