5bd5919f61540bdfd73c22cd2c34110c899f412a
Andreas Wilm - CMB5 <wilma@gis.a-star.edu.sg>
2015-02-06 10:49:00 +0800

    tests: valgrind call on indels and spike-in

--- tests/valgrind_call.sh
+++ tests/valgrind_call.sh
@@ -1,52 +1,75 @@
 #!/bin/bash
 
-# FIXME:add-doc
+# running valgrind on call incl indel calls on ecoli spikein
 
 source lib.sh || exit 1
 
-valgrind_log=$(mktemp -t $(basename $0).XXXXXX.valgrind)
-vcf_out=$(mktemp -t $(basename $0).XXXXXX.vcf)
-rm $vcf_out $valgrind_log
+KEEP_TMP=0
+BASEDIR=data/ecoli-clone/
+BAM=$BASEDIR/spike-in/spike-in.bam
+REF=$BASEDIR/ref/Ecoli_K12_MG1655_NC_000913.fa
 
-reffa=data/denv2-pseudoclonal/denv2-pseudoclonal_cons.fa
-bam=data/denv2-pseudoclonal/denv2-pseudoclonal.bam
-#reffa=./data/somatic/hg19_chr22.fa.gz
-#bam=./data/somatic/CHH966-tumor-100x-10pur-hg19.chr22-bed-only.bam
-# use region, otherwise execution will take ages
-#region_arg=consensus:100-300 # FIXME
+for f in $BAM $REF; do
+  if [ ! -e $f ]; then
+      echoerror "Required file $f missing"
+      exit 1
+  fi
+done
+            
 
-# use hard-coded bonf because:
-# 1. can't use auto since we don't have a bed-file
-# 2. can't use dynamic since that would eventually call a filtering
-# script which valgrind won'tlike
-bonf_arg="-b 308334"
-#region_arg="-r consensus:1-1000"
+outdir=$(mktemp -d -t $(basename $0).XXXXXX)
+log=$outdir/log.txt
+valgrindlog=$outdir/valgrind.log
+vcf_out=$outdir/out.vcf
 
-valgrind --suppressions=faidx_fetch_seq.supp --log-file=$valgrind_log --tool=memcheck --leak-check=full \
-    $LOFREQ call $region_arg $bonf_arg --no-default-filter -f $reffa -o $vcf_out $bam || exit 1
 
-num_snvs=$(grep -cv '^#' $vcf_out)
-if [ "$num_snvs" -lt 1 ]; then
-    echoerror "Found no SNVs in $vcf_out"
+# how to get a region with true SNVs and indels close-by
+#ipython
+#import vcf
+#vcfr = vcf.Reader(filename="truth.vcf.gz")
+#vars = [v for v in vcfr]
+#indel_highq = [v for v in vars_highq if v.is_indel]
+#snv_highq = [v for v in vars_highq if v.is_snp]
+#def argmin(iterable):
+#        return min(enumerate(iterable), key=lambda x: x[1])[0]
+#def closest(v, cmp_list):
+#    dists = [abs(v.POS-c.POS) for c in cmp_list]
+#    return argmin(dists)
+#for i in indel_highq:
+#    c = closest(i, snv_highq)
+#    print i, snv_highq[c]
+# and check that both are present in truth and lofreq prediction
+
+valgrind --suppressions=faidx_fetch_seq.supp --leak-check=full --tool=memcheck --log-file=$valgrindlog \
+  $LOFREQ call --call-indels -f $REF $BAM -r 'NC_000913:2000-2600' -o $vcf_out >$log 2>&1 || exit 1
+
+for pos in 2000 2032 2214 2514 2572; do 
+  if ! grep -q -w $pos $vcf_out; then
+    echoerror "Excepted variant position $pos not found in vcf $vcf_out"
     exit 1
-fi
+  fi
+done
+echook "All expected variant positions found"
 
-num_err=$(grep 'ERROR SUMMARY' $valgrind_log | grep -cv ': 0 errors')
+
+num_err=$(grep 'ERROR SUMMARY' $valgrindlog | grep -cv ': 0 errors')
 if [ "$num_err" -ne 0 ]; then
-    echoerror "Found errors in Valgrind output $valgrind_log"
+    echoerror "Found errors in Valgrind output $valgrindlog"
     exit 1
 else
     echook "No errors found in Valgrind output"
 fi
 
-lost_bytes=$(grep 'lost' $valgrind_log | grep -cv ': 0 bytes in 0 blocks')
+lost_bytes=$(grep 'lost' $valgrindlog | grep -cv ': 0 bytes in 0 blocks')
 if [ "$lost_bytes" -ne 0 ]; then
-    echoerror "Found lost bytes in Valgrind output $valgrind_log" || exit 1
+    echoerror "Found lost bytes in Valgrind output $valgrindlog" || exit 1
     exit 1
 else
     echook "No lost bytes found in Valgrind output"
 fi
 
-
-
-rm $vcf_out $valgrind_log
+if [ $KEEP_TMP -ne 1 ] && [ $num_err -eq 0 ]; then
+    test -d $outdir && rm -rf $outdir
+else
+    echowarn "Not deleting temporary output directory $outdir"
+fi
