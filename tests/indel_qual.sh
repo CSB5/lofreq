9bc1a967c38126fe03e3f72fa589ae88b6abb8cb
Andreas Wilm - CMB5 <wilma@gis.a-star.edu.sg>
2015-03-03 15:15:47 +0800

    indel_qual.sh: more stringent pvalue threshold to avoid fp

--- tests/indel_qual.sh
+++ tests/indel_qual.sh
@@ -8,7 +8,7 @@ set -o pipefail
 
 sam=data/idq/delq_3lq.sam
 ref=data/idq/ref.fa
-res=$(samtools view -bS $sam 2>/dev/null | $LOFREQ call --call-indels -f $ref  -b 1 -a 1 --no-default-filter -B -A - 2>&1 || exit 1)
+res=$(samtools view -bS $sam 2>/dev/null | $LOFREQ call --call-indels -f $ref  -b 1 -a 0.05 --no-default-filter -B -A - 2>&1 || exit 1)
 #echo "$res"
 if ! echo "$res" | grep -q 'ref[[:space:]]1'; then
 	echoerror "Should have called indel at pos 1 but didn't (res was $res)"
@@ -20,7 +20,7 @@ if echo "$res" | grep -q 'ref[[:space:]]3'; then
 fi
 
 sam=data/idq/delq_1lq.sam
-res=$(samtools view -bS $sam 2>/dev/null | $LOFREQ call --call-indels -f $ref  -b 1 -a 1 --no-default-filter -B -A - 2>&1 || exit 1)
+res=$(samtools view -bS $sam 2>/dev/null | $LOFREQ call --call-indels -f $ref  -b 1 -a 0.05 --no-default-filter -B -A - 2>&1 || exit 1)
 #echo "$res"
 if echo "$res" | grep -q 'ref[[:space:]]1'; then
         echoerror "Shouldn't have called indel at pos 1 but did (res was $res)"
